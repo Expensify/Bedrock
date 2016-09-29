b2c317904ae1f64907e6d13a25b07de3b14973d9
Tyler Karaszewski <tyler@expensify.com>
2016-09-29 13:52:32 -0700

    Add formatting script

---
+++ format.sh
@@ -0,0 +1,20 @@
+clang-format -i *.cpp
+clang-format -i *.h
+
+cd sqlitecluster
+find . -name "*.cpp" | xargs clang-format -i
+find . -name "*.h" | xargs clang-format -i
+cd ..
+
+cd libstuff
+find . -name "*.cpp" | xargs clang-format -i
+find . -name "*.h" | xargs clang-format -i
+cd ..
+
+cd plugins
+find . -name "*.cpp" | xargs clang-format -i
+find . -name "*.h" | xargs clang-format -i
+cd ..
+
+git checkout libstuff/sqlite3.h
+git checkout libstuff/sqlite3ext.h
