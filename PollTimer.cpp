966468a62f7ca153beda35fcf002457cf4fb14ae
Tyler Karaszewski <tyler@expensify.com>
2016-11-09 10:40:44 -0800

    Run auto-formatter on new files.

--- PollTimer.cpp
+++ PollTimer.cpp
@@ -1,12 +1,8 @@
 #include "PollTimer.h"
 
 PollTimer::PollTimer(uint64_t logIntervalSeconds)
-  : _logPeriod(logIntervalSeconds * STIME_US_PER_S),
-    _lastStart(0),
-    _lastStop(0),
-    _lastLogStart(0),
-    _timeInPoll(0),
-    _timeNotInPoll(0) { }
+    : _logPeriod(logIntervalSeconds * STIME_US_PER_S), _lastStart(0), _lastStop(0), _lastLogStart(0), _timeInPoll(0),
+      _timeNotInPoll(0) {}
 
 void PollTimer::startPoll() {
     uint64_t timestamp = STimeNow();
@@ -34,9 +30,8 @@ void PollTimer::stopPoll(bool force) {
         double percentage = 100.0 * ((double)_timeNotInPoll / (_timeInPoll + _timeNotInPoll));
         char buffer[7];
         snprintf(buffer, 7, "%.2f", percentage);
-        SINFO("[performance] " << (_timeInPoll + _timeNotInPoll) << "us elapsed, " << _timeInPoll
-              << "us in poll(), " << _timeNotInPoll << "us busy. " << buffer << "% busy."
-              << (force ? " (forced)" : ""));
+        SINFO("[performance] " << (_timeInPoll + _timeNotInPoll) << "us elapsed, " << _timeInPoll << "us in poll(), "
+                               << _timeNotInPoll << "us busy. " << buffer << "% busy." << (force ? " (forced)" : ""));
 
         // Reset this to our period after our previous start time, not after the current time, to prevent slow skew as
         // poll doesn't return precisely on these boundaries.
