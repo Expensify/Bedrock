36ae38dff91d2ee081c5352dca6b0f77fd516cf9
Tyler Karaszewski <tyler@expensify.com>
2017-04-06 15:31:37 -0700

    Update method signature

--- plugins/DB.h
+++ plugins/DB.h
@@ -4,10 +4,10 @@
 // Declare the class we're going to implement below
 class BedrockPlugin_DB : public BedrockPlugin {
   public:
-    string getName() { return "DB"; }
-    void initialize(const SData& args) { _args = args; }
-    bool peekCommand(SQLite& db, BedrockCommand& command);
-    bool processCommand(SQLite& db, BedrockCommand& command);
+    virtual string getName() { return "DB"; }
+    virtual void initialize(const SData& args, BedrockServer& server) { _args = args; }
+    virtual bool peekCommand(SQLite& db, BedrockCommand& command);
+    virtual bool processCommand(SQLite& db, BedrockCommand& command);
 
   private:
     // Attributes
