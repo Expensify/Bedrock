23e927ecf6a978ece173fe207e3ab345c163509d
Tyler Karaszewski <tyler@expensify.com>
2017-06-14 15:45:34 -0700

    First commit

---
+++ BedrockConflictManager.cpp
@@ -0,0 +1,81 @@
+#include "BedrockConflictManager.h"
+
+BedrockConflictInfo::BedrockConflictInfo(const string& commandName) :
+_commandName(commandName)
+{ }
+
+void BedrockConflictInfo::success() {
+    ++_totalSuccessCount;
+    _results.set(_resultsPtr);
+    ++_resultsPtr;
+    _resultsPtr %= COMMAND_COUNT;
+}
+
+void BedrockConflictInfo::conflict() {
+    ++_totalConflictCount;
+    _results.reset(_resultsPtr);
+    ++_resultsPtr;
+    _resultsPtr %= COMMAND_COUNT;
+}
+
+int BedrockConflictInfo::recentSuccessCount() const {
+    return _results.count();
+}
+
+int BedrockConflictInfo::recentConflictCount() const {
+    return COMMAND_COUNT - _results.count();
+}
+
+uint64_t BedrockConflictInfo::totalSuccessCount() const {
+    return _totalSuccessCount;
+}
+
+uint64_t BedrockConflictInfo::totalConflictCount() const {
+    return _totalConflictCount;
+}
+
+recursive_mutex BedrockConflictManager::_mutex;
+map<string, BedrockConflictInfo> BedrockConflictManager::_conflictInfoMap;
+int BedrockConflictManager::_threshold = (int)(BedrockConflictManager::_fraction * BedrockConflictInfo::COMMAND_COUNT);
+
+void BedrockConflictManager::commandConflicted(const string& commandName) {
+    SAUTOLOCK(_mutex);
+    // Look up, and create if necessary, the info object for this command.
+    auto it = _conflictInfoMap.find(commandName);
+    if (it == _conflictInfoMap.end()) {
+        // We need to insert this command.
+        auto itPair = _conflictInfoMap.emplace(make_pair(commandName, commandName));
+        it = itPair.first;
+    }
+    it->second.success();
+}
+
+void BedrockConflictManager::commandSucceeded(const string& commandName) {
+    SAUTOLOCK(_mutex);
+    // Look up, and create if necessary, the info object for this command.
+    auto it = _conflictInfoMap.find(commandName);
+    if (it == _conflictInfoMap.end()) {
+        // We need to insert this command.
+        auto itPair = _conflictInfoMap.emplace(make_pair(commandName, commandName));
+        it = itPair.first;
+    }
+    it->second.conflict();
+}
+
+bool BedrockConflictManager::multiWriteEnabled(const string& commandName) const {
+    SAUTOLOCK(_mutex);
+    auto it = _conflictInfoMap.find(commandName);
+    if (it == _conflictInfoMap.end()) {
+        SINFO("Command '" << commandName << "' not tracked in BedrockConflictManager. Assuming OK.");
+        return true;
+    }
+    int conflicts = it->second.recentConflictCount();
+    if (conflicts >= _threshold) {
+        SINFO("Not multi-writing command '" << commandName << "' because " << conflicts << " of the last "
+              << BedrockConflictInfo::COMMAND_COUNT << " calls have conflicted.");
+        return false;
+    }
+    SINFO("Multi-writing command '" << commandName << "'. Only " << conflicts << " of the last "
+          << BedrockConflictInfo::COMMAND_COUNT << " calls have conflicted.");
+    return true;
+}
---
+++ BedrockConflictManager.h
@@ -0,0 +1,70 @@
+#include <libstuff/libstuff.h>
+#include <bitset>
+
+class BedrockConflictInfo {
+public:
+    // The number of most recent commands to keep track of the results from.
+    static constexpr int COMMAND_COUNT = 100;
+
+    // Constructor takes the name of the command.
+    BedrockConflictInfo(const string& commandName);
+
+    // Call to increment successes.
+    void success();
+
+    // Call to increment conflicts.
+    void conflict();
+
+    // Returns the count of recent successes.
+    int recentSuccessCount() const;
+
+    // Returns the count of recent successes.
+    int recentConflictCount() const;
+
+    // Returns the count of all successful commits.
+    uint64_t totalSuccessCount() const;
+
+    // Returns the count of all conflicting commits.
+    uint64_t totalConflictCount() const;
+
+private:
+    // The results of the most recent COMMAND_COUNT commands.
+    bitset<COMMAND_COUNT> _results;
+
+    // Pointer to the next spot in _results to update.
+    int _resultsPtr = 0;
+
+    // Total counts of commands since creation.
+    uint64_t _totalSuccessCount;
+    uint64_t _totalConflictCount;
+
+    // Name of this command.
+    string _commandName;
+};
+
+class BedrockConflictManager {
+public:
+    // Caller should call this when an attempt was made to commit that resulted in a conflict.
+    void commandConflicted(const string& commandName);
+
+    // Caller should call this when an attempt was made to commit that succeeded.
+    void commandSucceeded(const string& commandName);
+
+    // Returns whether or not commands with this name are currently able to be written in parallel.
+    bool multiWriteEnabled(const string& commandName) const;
+
+private:
+    // Synchronization object.
+    static recursive_mutex _mutex;
+
+    // Map of commandNames to their BedrockConflictInfo objects.
+    static map<string, BedrockConflictInfo> _conflictInfoMap;
+
+    // The fraction of commands of a given name that are allowed to conflict before we decide the sync thread will
+    // process them all.
+    static constexpr double _fraction = 0.10;
+
+    // The number of conflicts of the last BedrockConflictInfo::COMMAND_COUNT commands that we'll allow to have failed
+    // before we decide that this command needs to be executed on the sync thread.
+    static int _threshold;
+};
