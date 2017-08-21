c6f2db5cc32b04c2f0ec22adc8067ae3e1afd48b
Tyler Karaszewski <tyler@expensify.com>
2017-08-21 10:52:56 -0700

    Fix status test

--- test/tests/StatusTest.cpp
+++ test/tests/StatusTest.cpp
@@ -17,7 +17,8 @@ struct StatusTest : tpunit::TestFixture {
         SData status("Status");
         string response = tester->executeWait(status);
         ASSERT_TRUE(SContains(response, "plugins"));
-        ASSERT_TRUE(SContains(response, "multiWriteWhiteList"));
+        ASSERT_TRUE(SContains(response, "multiWriteManualBlacklist"));
+        ASSERT_TRUE(SContains(response, "multiWriteAutoBlacklist"));
     }
 
 } __StatusTest;
