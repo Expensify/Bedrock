85af89750bc6438a8103e76348207aff416cf468
Tyler Karaszewski <tyler@expensify.com>
2017-10-11 17:03:54 -0700

    Done forget new test file.

---
+++ test/clustertest/tests/j_BadCommandTest.cpp
@@ -0,0 +1,111 @@
+#include "../BedrockClusterTester.h"
+
+struct j_BadCommandTest : tpunit::TestFixture {
+    j_BadCommandTest()
+        : tpunit::TestFixture("j_BadCommand",
+                              TEST(j_BadCommandTest::test)
+                             ) { }
+
+    BedrockClusterTester* tester;
+
+    void test()
+    {
+        tester = BedrockClusterTester::testers.front();
+        BedrockTester* master = tester->getBedrockTester(0);
+        BedrockTester* slave = tester->getBedrockTester(1);
+
+        bool diedCorrectly = false;
+        try {
+            SData cmd("dieinpeek");
+            cmd["userID"] = "31";
+            string response = master->executeWaitVerifyContent(cmd);
+        } catch (const SException& e) {
+            cout << "Looks like it died in peek (" << e.what() << ")." << endl;
+            diedCorrectly = (e.what() == "Empty response"s);
+        }
+
+        ASSERT_TRUE(diedCorrectly);
+
+        // Wait for something to be mastering.
+        sleep(1);
+
+        // Send the same command to a slave. It should blacklist it.
+        SData cmd("dieinpeek");
+        cmd["userID"] = "31";
+        string response = slave->executeWaitVerifyContent(cmd, "500 Blacklisted");
+        cout << "Slave blacklisted it ok" << endl;
+
+        // Try and bring master back up.
+        tester->startNode(0);
+        int count = 0;
+        bool success = false;
+        while (count++ < 50) {
+            SData cmd("Status");
+            string response = master->executeWaitVerifyContent(cmd);
+            STable json = SParseJSONObject(response);
+            if (json["state"] == "MASTERING") {
+                success = true;
+                break;
+            }
+
+            // Give it another second...
+            sleep(1);
+        }
+
+        ASSERT_TRUE(success);
+
+        cout << "Master is back up." << endl;
+
+        // Kill it in process.
+        diedCorrectly = false;
+        try {
+            SData cmd("dieinprocess");
+            cmd["userID"] = "32";
+            string response = master->executeWaitVerifyContent(cmd);
+        } catch (const SException& e) {
+            cout << "Looks like it died in process(" << e.what() << ")." << endl;
+            diedCorrectly = (e.what() == "Empty response"s);
+        }
+        ASSERT_TRUE(diedCorrectly);
+
+        // Wait until the old slave was mastering.
+        count = 0;
+        success = false;
+        while (count++ < 50) {
+            SData cmd("Status");
+            string response = slave->executeWaitVerifyContent(cmd);
+            STable json = SParseJSONObject(response);
+            if (json["state"] == "MASTERING") {
+                success = true;
+                break;
+            }
+
+            // Give it another second...
+            sleep(1);
+        }
+
+        ASSERT_TRUE(success);
+
+        cout << "Slave promoted to master." << endl;
+
+        // Send the same command to the slave (now master).
+        cmd = SData("dieinprocess");
+        cmd["userID"] = "32";
+        response = slave->executeWaitVerifyContent(cmd, "500 Blacklisted");
+
+        cout << "Promoted slave successfully blacklisted command." << endl;
+
+        // Kill it in process again with a different userID, since it won't count as blacklisted with a different user.
+        diedCorrectly = false;
+        try {
+            SData cmd("dieinprocess");
+            cmd["userID"] = "33";
+            string response = slave->executeWaitVerifyContent(cmd);
+        } catch (const SException& e) {
+            cout << "Looks like it died in process(" << e.what() << ")." << endl;
+            diedCorrectly = (e.what() == "Empty response"s);
+        }
+        ASSERT_TRUE(diedCorrectly);
+    }
+
+} __j_BadCommandTest;
