c62657e472a9fde1c07c2c87b63d25d25bf00cfa
Tyler Karaszewski <tyler@expensify.com>
2018-01-29 18:06:40 -0800

    Minor simplification

--- BedrockServer.cpp
+++ BedrockServer.cpp
@@ -1158,9 +1158,8 @@ void BedrockServer::postPoll(fd_map& fdm, uint64_t& nextActivity) {
     // Process any new activity from incoming sockets. In order to not modify the socket list while we're iterating
     // over it, we'll keep a list of sockets that need closing.
     list<STCPManager::Socket*> socketsToClose;
-    size_t socketListSize = socketList.size();
-    size_t deserializationAttempts = 0;
-    size_t deserializedRequests = 0;
+    int deserializationAttempts = 0;
+    int deserializedRequests = 0;
     uint64_t startTime = STimeNow();
     for (auto s : socketList) {
         switch (s->state.load()) {
@@ -1299,7 +1298,7 @@ void BedrockServer::postPoll(fd_map& fdm, uint64_t& nextActivity) {
 
     // Log the timing of this loop.
     uint64_t elapsedMS = (STimeNow() - startTime) / 1000;
-    SINFO("Read from " << socketListSize << " sockets, attempted to deserialize " << deserializationAttempts
+    SINFO("Read from " << socketList.size() << " sockets, attempted to deserialize " << deserializationAttempts
           << " commands, " << deserializedRequests << " were complete and deserialized. Took " << elapsedMS << "ms.");
 
     // Now we can close any sockets that we need to.
