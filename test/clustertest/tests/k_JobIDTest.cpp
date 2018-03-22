133633b005909802a261960f7af5007329cd9c8f
Ionatan Wiznia <ionatan@expensify.com>
2018-03-22 14:50:36 +0100

    Clean up jobs after test

--- test/clustertest/tests/k_JobIDTest.cpp
+++ test/clustertest/tests/k_JobIDTest.cpp
@@ -15,15 +15,22 @@ struct k_JobIDTest : tpunit::TestFixture {
         BedrockTester* slave = tester->getBedrockTester(1);
 
         // Create a job
-        SData cmd("CreateJob");
-        cmd["name"] = "TestJob";
-        STable response = master->executeWaitVerifyContentTable(cmd);
+        SData createCmd("CreateJob");
+        createCmd["name"] = "TestJob";
+        STable response = master->executeWaitVerifyContentTable(createCmd);
         const int jobID = SToInt(response["jobID"]);
 
         // Stop master
         tester->stopNode(0);
-        response = slave->executeWaitVerifyContentTable(cmd, "200");
+        response = slave->executeWaitVerifyContentTable(createCmd, "200");
         ASSERT_EQUAL(jobID + 1, SToInt(response["jobID"]));
+
+        // Get the 2 jobs to leave the db clean.
+        SData getCmd("GetJob");
+        getCmd["name"] = "TestJob";
+        getCmd["name"] = "*";
+        slave->executeWaitVerifyContentTable(getCmd, "200");
+        slave->executeWaitVerifyContentTable(getCmd, "200");
     }
 
 } __k_JobIDTest;
