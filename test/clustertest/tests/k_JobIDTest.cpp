578714e6f351448761730619d2e504e6c834009b
Ionatan Wiznia <ionatan@expensify.com>
2018-03-26 15:54:49 +0200

    Add more tests to jobID generation

--- test/clustertest/tests/k_JobIDTest.cpp
+++ test/clustertest/tests/k_JobIDTest.cpp
@@ -14,7 +14,7 @@ struct k_JobIDTest : tpunit::TestFixture {
         BedrockTester* master = tester->getBedrockTester(0);
         BedrockTester* slave = tester->getBedrockTester(1);
 
-        // Create a job
+        // Create a job in master
         SData createCmd("CreateJob");
         createCmd["name"] = "TestJob";
         STable response = master->executeWaitVerifyContentTable(createCmd);
@@ -22,18 +22,23 @@ struct k_JobIDTest : tpunit::TestFixture {
 
         // Stop master
         tester->stopNode(0);
+
+        // Create a job in the slave and check the ID returned is the next one
         response = slave->executeWaitVerifyContentTable(createCmd, "200");
         ASSERT_EQUAL(jobID + 1, SToInt(response["jobID"]));
 
-        // Get the 2 jobs to leave the db clean.
-        SData getCmd("GetJob");
-        getCmd["name"] = "TestJob";
-        getCmd["name"] = "*";
-        slave->executeWaitVerifyContentTable(getCmd, "200");
-        slave->executeWaitVerifyContentTable(getCmd, "200");
-
-        // Restart master so rest of tests can use it.
+        // Restart master
         tester->startNode(0);
+
+        // Create a new job in master and check the ID is the next one
+        response = master->executeWaitVerifyContentTable(createCmd);
+        ASSERT_EQUAL(jobID + 2, SToInt(response["jobID"]));
+
+        // Get the 3 jobs to leave the db clean
+        SData getCmd("GetJobs");
+        getCmd["name"] = "*";
+        getCmd["numResults"] = 3;
+        slave->executeWaitVerifyContentTable(getCmd, "200");
     }
 
 } __k_JobIDTest;
