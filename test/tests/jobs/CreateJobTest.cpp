3e7bc81e09349cf819bc6258c7fe370279efbe46
Qadir Haqq <qadir@expensify.com>
2018-09-25 17:06:14 +0200

    Removed unit tests for preventing retryAfter

--- test/tests/jobs/CreateJobTest.cpp
+++ test/tests/jobs/CreateJobTest.cpp
@@ -17,7 +17,6 @@ struct CreateJobTest : tpunit::TestFixture {
                               TEST(CreateJobTest::createChildWithRunningGrandparent),
                               TEST(CreateJobTest::retryRecurringJobs),
                               TEST(CreateJobTest::retryWithMalformedValue),
-                              TEST(CreateJobTest::retryUnique),
                               TEST(CreateJobTest::retryLifecycle),
                               TEST(CreateJobTest::retryWithChildren),
                               AFTER(CreateJobTest::tearDown),
@@ -278,14 +277,6 @@ struct CreateJobTest : tpunit::TestFixture {
         command["name"] = "test";
         command["retryAfter"] = "10";
         tester->executeWaitVerifyContent(command, "402 Malformed retryAfter");
-    }
-
-    void retryUnique() {
-        SData command("CreateJob");
-        command["name"] = "test";
-        command["retryAfter"] = "+10 HOUR";
-        command["unique"] = "true";
-        tester->executeWaitVerifyContent(command, "405 Unique jobs can't be retried");
     }
 
     void retryLifecycle() {
