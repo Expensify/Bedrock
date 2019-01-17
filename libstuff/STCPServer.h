68f5cd615f45e8820dfed2a47a5121238438a560
Jerry Wardlow <jerry@jerryoftheyear.com>
2019-01-16 17:46:50 -0800

    Revert "Revert "Merge pull request #578 from Expensify/tyler-multi-network-2""

--- libstuff/STCPServer.h
+++ libstuff/STCPServer.h
@@ -23,7 +23,7 @@ struct STCPServer : public STCPManager {
     void closePorts(list<Port*> except = {});
 
     // Tries to accept a new incoming socket
-    Socket* acceptSocket(Port*& port);
+    Socket* acceptSocket(Port*& port, bool deferRead = false);
     Socket* acceptSocket() {
         Port* ignore;
         return acceptSocket(ignore);
@@ -37,5 +37,5 @@ struct STCPServer : public STCPManager {
     list<Port> portList;
 
     // Protect access to to the port list when multiple threads insert and delete from it.
-    mutex portListMutex;
+    recursive_mutex portListMutex;
 };
