ce449c8ae552b667c76c6e29d52dd2852ab46c83
Cole Eason <cole@expensify.com>
2019-01-18 22:15:58 -0800

    Check peer socket exists before adding logging param

--- sqlitecluster/SQLiteNode.cpp
+++ sqlitecluster/SQLiteNode.cpp
@@ -100,7 +100,9 @@ void SQLiteNode::sendResponse(const SQLiteCommand& command)
     escalate["ID"] = command.id;
     escalate.content = command.response.serialize();
     SINFO("Sending ESCALATE_RESPONSE to " << peer->name << " for " << command.id << ".");
-    peer->s->logString = "ESCALATE_RESPONSE " + command.id;
+    if (peer->s) {
+        peer->s->logString = "ESCALATE_RESPONSE " + command.id;
+    }
     _sendToPeer(peer, escalate);
 }
 
