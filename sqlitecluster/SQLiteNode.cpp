13cf09c96ed5eac1f5754e5ec1320fa45b29ce85
Cole Eason <cole@expensify.com>
2019-01-28 12:04:43 -0800

    Add ESCALATE_RESPONSE logging and send() timings

--- sqlitecluster/SQLiteNode.cpp
+++ sqlitecluster/SQLiteNode.cpp
@@ -100,6 +100,7 @@ void SQLiteNode::sendResponse(const SQLiteCommand& command)
     SData escalate("ESCALATE_RESPONSE");
     escalate["ID"] = command.id;
     escalate.content = command.response.serialize();
+    SINFO("Sending ESCALATE_RESPONSE to " << peer->name << " for " << command.id << ".");
     _sendToPeer(peer, escalate);
 }
 
