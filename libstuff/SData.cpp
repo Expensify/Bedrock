b898111ff9126f84f9eccbceff1c744d711b3363
Cole Eason <cole@expensify.com>
2020-03-09 17:23:17 +0000

    Make SData constructor deserialize by default, clean up file

--- libstuff/SData.cpp
+++ libstuff/SData.cpp
@@ -1,26 +1,26 @@
 #include "libstuff.h"
 
 const string SData::placeholder;
-// --------------------------------------------------------------------------
+
 SData::SData() {
     // Nothing to do here
 }
 
-// --------------------------------------------------------------------------
-SData::SData(const string& method) {
-    // Initialize the method
-    methodLine = method;
+// Initializes a new SData from a string.  If the string provided is not
+// an entire HTTPs like message, the string is used as the methodLine.
+SData::SData(const string& rhs) {
+    if(!SParseHTTP(rhs, methodLine, nameValueMap, content)){
+        methodLine = rhs;
+    }
 }
 
-// --------------------------------------------------------------------------
+// This version creates an entry, if necessary, and returns a reference
 string& SData::operator[](const string& name) {
-    // This version creates an entry, if necessary, and returns a reference
     return nameValueMap[name];
 }
 
-// --------------------------------------------------------------------------
+// This version takes care not to create an entry if none is present
 const string& SData::operator[](const string& name) const {
-    // This version takes care not to create an entry if none is present
     STable::const_iterator it = nameValueMap.find(name);
     if (it == nameValueMap.end()) {
         return placeholder;
@@ -29,54 +29,46 @@ const string& SData::operator[](const string& name) const {
     }
 }
 
-// --------------------------------------------------------------------------
+// Erase everything
 void SData::clear() {
-    // Erase everything
     methodLine.clear();
     nameValueMap.clear();
     content.clear();
 }
 
-// --------------------------------------------------------------------------
+// Erase one value
 void SData::erase(const string& name) {
-    // Erase one value
     nameValueMap.erase(name);
 }
 
-// --------------------------------------------------------------------------
+// Combine the name/value pairs from two datas
 void SData::merge(const STable& rhs) {
-    // Combine the name/value pairs from two datas
     nameValueMap.insert(rhs.begin(), rhs.end());
 }
 
-// --------------------------------------------------------------------------
+// Combine two SData into one
 void SData::merge(const SData& rhs) {
-    // Combine two SData into one
     // **FIXME: What do we do with the content?  Where do we use this?
     merge(rhs.nameValueMap);
 }
 
-// --------------------------------------------------------------------------
+// Returns whether or not this data is empty
 bool SData::empty() const {
-    // Returns whether or not this data is empty
     return (methodLine.empty() && nameValueMap.empty() && content.empty());
 }
 
-// --------------------------------------------------------------------------
+// Returns whether or not a particular value has been set
 bool SData::isSet(const string& name) const {
-    // Returns whether or not a particular value has been set
     return SContains(nameValueMap, name);
 }
 
-// --------------------------------------------------------------------------
+// Return as a 32-bit value.
 int SData::calc(const string& name) const {
-    // Forcing 32 bitness here.
     return min((long)calc64(name), (long)0x7fffffffL);
 }
 
-// --------------------------------------------------------------------------
+// Return as a 64-bit value
 int64_t SData::calc64(const string& name) const {
-    // Return as a 64-bit value
     STable::const_iterator it = nameValueMap.find(name);
     if (it == nameValueMap.end()) {
         return 0;
@@ -85,9 +77,8 @@ int64_t SData::calc64(const string& name) const {
     }
 }
 
-// --------------------------------------------------------------------------
+// Return as an unsigned 64-bit value
 uint64_t SData::calcU64(const string& name) const {
-    // Return as an unsigned 64-bit value
     STable::const_iterator it = nameValueMap.find(name);
     if (it == nameValueMap.end()) {
         return 0;
@@ -96,47 +87,42 @@ uint64_t SData::calcU64(const string& name) const {
     }
 }
 
-// --------------------------------------------------------------------------
+// Returns if the value evaluates to true
 bool SData::test(const string& name) const {
-    // Returns if the value evaluates to true
     const string& value = (*this)[name];
     return (SIEquals(value, "true") || calc(name) != 0);
 }
 
-// --------------------------------------------------------------------------
+// Gets the verb (eg "GET") from the method line
 string SData::getVerb() const {
-    // Gets the verb (eg "GET") from the method line
     return methodLine.substr(0, methodLine.find(" "));
 }
 
-// --------------------------------------------------------------------------
+// Serializes this to an ostringstream
 void SData::serialize(ostringstream& out) const {
-    // Serializes this to an ostringstream
     out << SComposeHTTP(methodLine, nameValueMap, content);
 }
 
-// --------------------------------------------------------------------------
+// Serializes this to a string
 string SData::serialize() const {
     // Serializes this to a string
     return SComposeHTTP(methodLine, nameValueMap, content);
 }
 
-// --------------------------------------------------------------------------
+// Deserializes from a string
 int SData::deserialize(const string& rhs) {
-    // Deserializes from a string
     return (SParseHTTP(rhs, methodLine, nameValueMap, content));
 }
 
-// --------------------------------------------------------------------------
+// Deserializes from a buffer
 int SData::deserialize(const char* buffer, int length) {
-    // Deserializes from a buffer
     return (SParseHTTP(buffer, length, methodLine, nameValueMap, content));
 }
 
-// --------------------------------------------------------------------------
+// Initializes a new SData from a string.  If there is no content provided,
+// then use whatever data remains in the string as the content
+// **DEPRECATED** Use the constructor that handles this instead.
 SData SData::create(const string& rhs) {
-    // Initializes a new SData from a string.  If there is no content provided,
-    // then use whatever data remains in the string as the content
     SData data;
     int header = data.deserialize(rhs);
     if (header && data.content.empty()) {
