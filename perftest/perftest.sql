2626b79000ef6ffe4434eac0a633efdb9b204ca8
Cole Eason <cole@expensify.com>
2020-04-01 21:26:26 +0000

    Update default perftestsql rows

--- perftest/perftest.sql
+++ perftest/perftest.sql
@@ -7,11 +7,10 @@ WITH RECURSIVE
         SELECT RANDOM(), RANDOM()
             UNION ALL
         SELECT RANDOM(), RANDOM() FROM randdata
-        LIMIT 1000*1000*1
+        LIMIT 100000*100000*1 -- makes 10b row, roughly a 250gb, database
     )
 
 INSERT INTO perfTest ( indexedColumn, nonIndexedColumn )
     SELECT * FROM randdata;
 
 CREATE INDEX perfTestIndexedColumn ON perfTest ( indexedColumn );
-
