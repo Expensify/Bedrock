3bf04af57040b309abf89f3b6d0579f1dad7b8a0
Cole Eason <cole@expensify.com>
2020-04-01 20:36:58 +0000

    Pull in perf tests, update make file, remove build.sh, update gitignore

---
+++ perftest/perftest.sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS perfTest;
+
+CREATE TABLE perfTest ( indexedColumn, nonIndexedColumn );
+
+WITH RECURSIVE
+    randdata(x, y) AS (
+        SELECT RANDOM(), RANDOM()
+            UNION ALL
+        SELECT RANDOM(), RANDOM() FROM randdata
+        LIMIT 1000*1000*1
+    )
+
+INSERT INTO perfTest ( indexedColumn, nonIndexedColumn )
+    SELECT * FROM randdata;
+
+CREATE INDEX perfTestIndexedColumn ON perfTest ( indexedColumn );
+
