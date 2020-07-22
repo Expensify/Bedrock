b5e4fdeaae650566bb02d6c96f8d2616129f89f1
Tyler Karaszewski <tyler@expensify.com>
2020-07-22 10:54:37 -0700

    typo

--- BedrockServer.cpp
+++ BedrockServer.cpp
@@ -181,7 +181,7 @@ void BedrockServer::sync(const SData& args,
     int64_t mmapSizeGB = args.isSet("-mmapSizeGB") ? stoll(args["-mmapSizeGB"]) : 0;
 
     // We allocate 25% of the total available file handles to our DB pool. Note that on Linux, the default limit is only
-    // 2014 file handles, so this can be as low as 256 if not adjusted elsewhere (it's adjusted in main.cpp for live
+    // 1024 file handles, so this can be as low as 256 if not adjusted elsewhere (it's adjusted in main.cpp for live
     // servers).
     struct rlimit limits;
     if (getrlimit(RLIMIT_NOFILE, &limits)) {
