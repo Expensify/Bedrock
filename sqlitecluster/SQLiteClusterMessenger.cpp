c27f3b0535d3c59a6d48961bbf83fdca40158a46
Tyler Karaszewski <tyler@expensify.com>
2022-02-15 10:39:00 -0400

    Add mutex protecting transaction commands map

--- sqlitecluster/SQLiteClusterMessenger.cpp
+++ sqlitecluster/SQLiteClusterMessenger.cpp
@@ -11,9 +11,12 @@ bool SQLiteClusterMessenger::sendToLeader(BedrockCommand& command) {
     string leaderAddress;
     auto _nodeCopy = atomic_load(&_node);
     if (_nodeCopy) {
+        // peerList is const, so we can safely read from it in  multiple threads without locking, similarly,
+        // peer->commandAddress is atomic.
         for (SQLiteNode::Peer* peer : _nodeCopy->peerList) {
-            if (peer->state == STCPNode::LEADING && !peer->commandAddress.load().empty()) {
-                leaderAddress = peer->commandAddress;
+            string peerCommandAddress = peer->commandAddress;
+            if (peer->state == STCPNode::LEADING && !peerCommandAddress.empty()) {
+                leaderAddress = peerCommandAddress;
                 break;
             }
         }
@@ -29,13 +32,16 @@ bool SQLiteClusterMessenger::sendToLeader(BedrockCommand& command) {
     // Create a new transaction. This can throw if `validate` fails. We explicitly do this *before* creating a socket.
     Transaction* transaction = new Transaction(*this);
 
-    // I don't trust this not to ever leak currently, but for the moment, this is OK.
-    _transactionCommands[transaction] = make_pair(&command, STimeNow());
+    {
+        lock_guard<mutex> lock(_transactionCommandMutex);
+        _transactionCommands[transaction] = make_pair(&command, STimeNow());
+    }
 
     Socket* s = nullptr;
     try {
         s = new Socket(host, nullptr);
     } catch (const SException& exception) {
+        lock_guard<mutex> lock(_transactionCommandMutex);
         _transactionCommands.erase(transaction);
         delete transaction;
         return false;
@@ -55,6 +61,7 @@ bool SQLiteClusterMessenger::sendToLeader(BedrockCommand& command) {
 bool SQLiteClusterMessenger::_onRecv(Transaction* transaction)
 {
     transaction->response = getHTTPResponseCode(transaction->fullResponse.methodLine);
+    lock_guard<mutex> lock(_transactionCommandMutex);
     auto cmdIt = _transactionCommands.find(transaction);
     if (cmdIt != _transactionCommands.end()) {
         BedrockCommand* command = cmdIt->second.first;
