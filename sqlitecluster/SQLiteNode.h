669edc4f989b05d1971041e0ca32d88165a27642
Tyler Karaszewski <tyler@expensify.com>
2022-04-27 16:54:28 -0700

    Comment

--- sqlitecluster/SQLiteNode.h
+++ sqlitecluster/SQLiteNode.h
@@ -20,6 +20,17 @@
 // 7. non-const instance members.
 // 8. In each of these sections, things should be alphabetized.
 
+/*
+ * Rules for maintaining SQLiteNode methods so that atomicity works as intended.
+ *
+ * No non-const members should be publicly exposed.
+ * Any public method that is `const` must shared_lock<>(nodeMutex).
+ * Any pubic method that is non-const must uniaue_lock<>(nodeMutex).
+ * Any private methods must not call public methods.
+ * Any private methods must not lock nodeMutex (for recursion reasons).
+ * Any public methods must not call other public methods.
+ */
+
 // Diagnostic class for timing what fraction of time happens in certain blocks.
 // TODO: Move out of SQLiteNode.
 class AutoTimer {
