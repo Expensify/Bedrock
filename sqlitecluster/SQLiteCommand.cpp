3ea733018dfb78fea0a42894de30cf256aa1a8af
Tyler Karaszewski <tyler@tylerkaraszewski.com>
2022-05-06 01:02:52 -0400

    Revert "Remove option to turn off HTTP escalation"

--- sqlitecluster/SQLiteCommand.cpp
+++ sqlitecluster/SQLiteCommand.cpp
@@ -24,6 +24,7 @@ SData SQLiteCommand::preprocessRequest(SData&& request) {
 SQLiteCommand::SQLiteCommand(SData&& _request) : 
     privateRequest(move(preprocessRequest(move(_request)))),
     request(privateRequest),
+    initiatingPeerID(0),
     initiatingClientID(0),
     writeConsistency(SQLiteNode::ASYNC),
     complete(false),
@@ -53,6 +54,7 @@ SQLiteCommand::SQLiteCommand(SData&& _request) :
 SQLiteCommand::SQLiteCommand(SQLiteCommand&& from) : 
     privateRequest(move(from.privateRequest)),
     request(privateRequest),
+    initiatingPeerID(from.initiatingPeerID),
     initiatingClientID(from.initiatingClientID),
     id(move(from.id)),
     jsonContent(move(from.jsonContent)),
@@ -68,6 +70,7 @@ SQLiteCommand::SQLiteCommand(SQLiteCommand&& from) :
 SQLiteCommand::SQLiteCommand() :
     privateRequest(),
     request(privateRequest),
+    initiatingPeerID(0),
     initiatingClientID(0),
     writeConsistency(SQLiteNode::ASYNC),
     complete(false),
