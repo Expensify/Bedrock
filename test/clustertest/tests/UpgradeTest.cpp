10a41814c877196669c01245d2b392ec6027292e
Tyler Karaszewski <tyler@expensify.com>
2022-05-09 15:00:54 -0700

    Cleanup and basic test

--- test/clustertest/tests/UpgradeTest.cpp
+++ test/clustertest/tests/UpgradeTest.cpp
@@ -10,24 +10,17 @@ struct UpgradeTest : tpunit::TestFixture {
                              ) { }
 
     BedrockClusterTester* tester;
+    string prodBedrockName;
 
     void setup() {
-        // Get a directory we can work in.
-        char brReleaseDirArr[] = "/tmp/br-prod-test-XXXXXX";
-        mkdtemp(brReleaseDirArr);
-        string brReleaseDir(brReleaseDirArr, sizeof(brReleaseDirArr) - 1);
-        cout << "Using temp dir for prod bedrock: " << brReleaseDir << endl;
-
         // Get the list of recent releases.
         const string tempJson = "brdata.json";
-        string command = "curl --silent 'https://api.github.com/repos/Expensify/Bedrock/releases?page=1&per_page=1' -o " + brReleaseDir + "/" + tempJson;
-        cout << command << endl;
-        if (int result = system(command.c_str())) {
-            cout << "Couldn't download bedrock releases: " << result << ":" << errno << endl;
-        }
+        string command = "curl --silent 'https://api.github.com/repos/Expensify/Bedrock/releases?page=1&per_page=1' -o " + tempJson;
+        ASSERT_FALSE(system(command.c_str()));
 
         // Parse a tag from it.
-        string data = SFileLoad(brReleaseDir + "/" + tempJson);
+        string data = SFileLoad(tempJson);
+        SFileDelete(tempJson);
         string bedrockTagName;
         list<string> j1 = SParseJSONArray(STrim(data));
         if (j1.size()) {
@@ -38,27 +31,33 @@ struct UpgradeTest : tpunit::TestFixture {
             }
         }
 
-        // Clone bedrock
-        if (system(("cd " + brReleaseDir + " && git clone https://github.com/Expensify/Bedrock.git > /dev/null").c_str())) {
-            cout << "Couldn't clone Bedrock" << endl;
-            return;
+        // HACK! This just gets known broken version. Delete when done.
+        // bedrockTagName = "2022-05-06";
+
+        // If we've already built this, don't bother doing it again. This makes running this test multiple times in a
+        // row much faster.
+        prodBedrockName = "/tmp/bedrock-" + bedrockTagName; 
+        if (!SFileExists(prodBedrockName)) {
+            // Get a directory we can work in.
+            char brReleaseDirArr[] = "/tmp/br-prod-test-XXXXXX";
+            mkdtemp(brReleaseDirArr);
+            string brReleaseDir(brReleaseDirArr, sizeof(brReleaseDirArr) - 1);
+
+            // Clone bedrock.
+            ASSERT_FALSE(system(("cd " + brReleaseDir + " && git clone https://github.com/Expensify/Bedrock.git > /dev/null").c_str()));
+
+            // Check out the release tag.
+            ASSERT_FALSE(system(("cd " + brReleaseDir + " && cd Bedrock && git checkout " + bedrockTagName + "  > /dev/null").c_str()));
+
+            // Build the release.
+            ASSERT_FALSE(system(("cd " + brReleaseDir + " && cd Bedrock && make -j8 bedrock  > /dev/null").c_str()));
+
+            // Save the final product.
+            ASSERT_FALSE(system(("mv " + brReleaseDir + "/Bedrock/bedrock " + prodBedrockName).c_str()));
         }
 
-        // Check out the release tag.
-        if (system(("cd " + brReleaseDir + " && cd Bedrock && git checkout " + bedrockTagName + "  > /dev/null").c_str())) {
-            cout << "Couldn't heckout latest release" << endl;
-            return;
-        }
-
-        // Build the release.
-        if (system(("cd " + brReleaseDir + " && cd Bedrock && make -j8 bedrock  > /dev/null").c_str())) {
-            cout << "Couldn't build release bedrock" << endl;
-            return;
-        }
-
-        tester = new BedrockClusterTester("db,cache,jobs", brReleaseDir + "/Bedrock/bedrock");
-        cout << "Starting prod bedrock cluster" << endl;
-        sleep(10);
+        cout << "Starting prod version at " << prodBedrockName << endl;
+        tester = new BedrockClusterTester("db,cache,jobs", prodBedrockName);
     }
 
     void teardown() {
@@ -66,23 +65,29 @@ struct UpgradeTest : tpunit::TestFixture {
     }
 
     void test() {
+        // Verify 0 is leader.
         ASSERT_TRUE(tester->getTester(0).waitForState("LEADING"));
-        cout << "Leader is up, restarting follower 2." << endl;
 
+        // Restart 2 on the new version.
         tester->getTester(2).stopServer();
         tester->getTester(2).serverName = "bedrock";
-        
-        cout << "Bringing up new version as follower" << endl;
         tester->getTester(2).startServer();
         ASSERT_TRUE(tester->getTester(2).waitForState("FOLLOWING"));
-        cout << "New version is following" << endl;
 
+        // Verify 0 and 1 are the same version, but 2 is a different version.
         SData status("Status");
-        for (auto i: {0, 2}) {
+        vector<string> versions(3);
+        for (auto i: {0, 1, 2}) {
             vector<SData> statusResult = tester->getTester(i).executeWaitMultipleData({status});
-            cout << statusResult[0].serialize() << endl;
+            versions[i] = SParseJSONObject(statusResult[0].content)["version"];
         }
+        ASSERT_EQUAL(versions[0], versions[1]);
+        ASSERT_NOT_EQUAL(versions[0], versions[2]);
 
+        // Send a write command on 2 and verify we get a response.
+        SData cmd("ineffectiveUpdate");
+        vector<SData> cmdResult = tester->getTester(2).executeWaitMultipleData({cmd});
+        ASSERT_EQUAL(cmdResult[0].methodLine, "200 OK");
     }
 
 } __UpgradeTest;
