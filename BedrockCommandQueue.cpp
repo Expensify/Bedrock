610e4c624322c09159c1314e957fb84db74ce024
Tyler Karaszewski <tyler@expensify.com>
2022-06-01 15:27:12 -0700

    Remove unneeded code

--- BedrockCommandQueue.cpp
+++ BedrockCommandQueue.cpp
@@ -65,11 +65,5 @@ void BedrockCommandQueue::abandonFutureCommands(int msInFuture) {
 }
 
 void BedrockCommandQueue::push(unique_ptr<BedrockCommand>&& command) {
-    BedrockCommand::Priority priority = command->priority;
-    uint64_t executionTime = command->request.calcU64("commandExecuteTime");
-    if (!executionTime) {
-        executionTime = STimeNow();
-    }
-    uint64_t timeout = command->timeout();
-    SScheduledPriorityQueue<unique_ptr<BedrockCommand>>::push(move(command), priority, command->scheduledTime, timeout);
+    SScheduledPriorityQueue<unique_ptr<BedrockCommand>>::push(move(command), command->priority, command->scheduledTime, command->timeout());
 }
