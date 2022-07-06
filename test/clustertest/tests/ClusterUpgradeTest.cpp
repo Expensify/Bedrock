095204a92f41fb4d39106c50ac4dc3cb485181ed
Tyler Karaszewski <tyler@expensify.com>
2022-07-06 11:40:50 -0700

    Needs single quotes

--- test/clustertest/tests/ClusterUpgradeTest.cpp
+++ test/clustertest/tests/ClusterUpgradeTest.cpp
@@ -47,11 +47,9 @@ struct ClusterUpgradeTest : tpunit::TestFixture {
         // the commit number of the tag: git rev-list -n 1 $TAG
         // The commit number we're currently on: git rev-parse HEAD
         // If the current commit matches the latest tag commit, our script returns 1 and we will test against the second tag. Otherwise, it returns 0 and we test against the first tag.
-        string checkIfOnLatestTag = "/bin/bash -c if [ \"`git rev-list -n 1 " + tagNames[0] + "`\" = \"`git rev-parse HEAD`\" ]; then exit 1; else exit 0; fi";
+        string checkIfOnLatestTag = "/bin/bash -c 'if [ \"`git rev-list -n 1 " + tagNames[0] + "`\" = \"`git rev-parse HEAD`\" ]; then exit 1; else exit 0; fi'";
         int result = system(checkIfOnLatestTag.c_str());
         bedrockTagName = tagNames[result];
-
-        cout << "Testing against: " << bedrockTagName << endl;
 
         // If you'd like to test against a particular tag, uncomment the following line. The value chosen here was a
         // known bad version that failed to escalate commands at upgrade when first deployed.
