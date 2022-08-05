8b951a66836a44a0445e8960228175aba521a525
Tyler Karaszewski <tyler@expensify.com>
2022-08-05 15:47:22 -0700

    Typo

--- BedrockServer.cpp
+++ BedrockServer.cpp
@@ -2286,7 +2286,7 @@ void BedrockServer::handleSocket(Socket&& socket, bool fromControlPort, bool fro
                             // If this command is scheduled in the future, we can't just run it, we need to enqueue it to run at that point.
                             // This functionality will go away as we remove the queues from bedrock, and so this can be removed at that time.
                             if (command->request.calcU64("commandExecuteTime") > STimeNow()) {
-                                // These are implictly fire-and-forget commands and a response will already have been sent in `buildCommandFromRequest`.
+                                // These are implicitly fire-and-forget commands and a response will already have been sent in `buildCommandFromRequest`.
                                 _commandQueue.push(move(command));
                                 continue;
                             } else {
