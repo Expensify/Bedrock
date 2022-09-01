36c2442260b8dcdd16fa13d90da8fcc664c0896a
Tyler Karaszewski <tyler@expensify.com>
2022-09-01 17:48:59 +0200

    Add a command that doesn't conflict on purpose

--- test/clustertest/testplugin/TestPlugin.cpp
+++ test/clustertest/testplugin/TestPlugin.cpp
@@ -5,6 +5,7 @@
 #include <string.h>
 
 #include <libstuff/SQResult.h>
+#include <libstuff/SRandom.h>
 #include <libstuff/SX509.h>
 
 mutex BedrockPlugin_TestPlugin::dataLock;
@@ -92,6 +93,7 @@ unique_ptr<BedrockCommand> BedrockPlugin_TestPlugin::getCommand(SQLiteCommand&& 
         "ineffectiveUpdate",
         "exceptioninprocess",
         "generatesegfaultprocess",
+        "randominsert",
         "idcollision",
         "slowprocessquery",
     };
@@ -226,6 +228,8 @@ bool TestPluginCommand::peek(SQLite& db) {
             db.read(query, result);
         }
         return true;
+    } else if (SStartsWith(request.methodLine, "randominsert")) {
+        // Go to process.
     } else if (SStartsWith(request.methodLine, "idcollision")) {
         usleep(1001); // for TimingTest to not get 0 values.
     } else if (SStartsWith(request.methodLine, "httpstimeout")) {
@@ -364,6 +368,10 @@ void TestPluginCommand::process(SQLite& db) {
         }
 
         // Done.
+        return;
+    } else if (SStartsWith(request.methodLine, "randominsert")) {
+        int64_t randomSignedInt = SRandom::rand64() >> 1;
+        SASSERT(db.write("INSERT INTO TEST VALUES(" + SQ(randomSignedInt) + ", " + SQ(request["value"]) + ");"));
         return;
     } else if (SStartsWith(request.methodLine, "idcollision")) {
         usleep(1001); // for TimingTest to not get 0 values.
