f1de79d3d20e7ec3d56fcbfa3712cd46f7410c88
Tyler Karaszewski <tyler@expensify.com>
2023-03-02 12:20:45 -0800

    Fix timingTest but old lastPeekedOrProcessedInState check is broken

--- test/clustertest/tests/TimingTest.cpp
+++ test/clustertest/tests/TimingTest.cpp
@@ -29,31 +29,20 @@ struct TimingTest : tpunit::TestFixture {
                     continue;
                 }
             }
-            /* Uncomment for inspection.
-            for (const auto& row : result.nameValueMap) {
-                cout << "[TimingTest] " << row.first << ":" << row.second << endl;
-            }
-            cout << "[TimingTest] " << endl;
-            */
 
             uint64_t peekTime = SToUInt64(result["peekTime"]);
             uint64_t processTime = SToUInt64(result["processTime"]);
             uint64_t totalTime = SToUInt64(result["totalTime"]);
 
-            // Leader should have peek and process times, followers only peek.
-            if (i == 0) {
-                if (peekTime <= 0 || processTime <= 0) {
-                    cout << "[TimingTest] peekTime: " << peekTime << endl;
-                    cout << "[TimingTest] processTime: " << processTime << endl;
-                    cout << "[TimingTest] totalTime: " << totalTime << endl;
-                    cout << "[TimingTest] " << result.serialize() << endl;
-                }
-                ASSERT_GREATER_THAN(peekTime, 0);
-                ASSERT_GREATER_THAN(processTime, 0);
-            } else {
-                ASSERT_GREATER_THAN(peekTime, 0);
-                ASSERT_EQUAL(processTime, 0);
+            // All servers should get peek and process times.
+            if (peekTime <= 0 || processTime <= 0) {
+                cout << "[TimingTest] peekTime: " << peekTime << endl;
+                cout << "[TimingTest] processTime: " << processTime << endl;
+                cout << "[TimingTest] totalTime: " << totalTime << endl;
+                cout << "[TimingTest] " << result.serialize() << endl;
             }
+            ASSERT_GREATER_THAN(peekTime, 0);
+            ASSERT_GREATER_THAN(processTime, 0);
 
             if (peekTime + processTime >= totalTime) {
                 // These are just blank in the failure case.
