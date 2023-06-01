c8209b705f262302f48dcc8af048c9205d94ddd7
Joel Bettner <joelbettner@expensify.com>
2023-05-31 21:37:57 -0600

    Add actual tests to make sure prePeek and postProcess data gets saved correctly

--- test/clustertest/tests/PrePeekPostProcessTest.cpp
+++ test/clustertest/tests/PrePeekPostProcessTest.cpp
@@ -21,14 +21,27 @@ struct PrePeekPostProcessTest : tpunit::TestFixture {
 
     void prePeek()
     {
+        BedrockTester& brtester = tester->getTester(1);
+        SData cmd("prepeekcommand");
+        STable response = SParseJSONObject(brtester.executeWaitMultipleData({cmd})[0].content);
+        ASSERT_EQUAL(response["prePeekInfo"], "this was returned in prePeekInfo");
     }
 
     void postProcess()
     {
+        BedrockTester& brtester = tester->getTester(1);
+        SData cmd("postprocesscommand");
+        STable response = SParseJSONObject(brtester.executeWaitMultipleData({cmd})[0].content);
+        ASSERT_EQUAL(response["postProcessInfo"], "this was returned in postProcessInfo");
     }
 
     void prePeekPostProcess ()
     {
+        BedrockTester& brtester = tester->getTester(1);
+        SData cmd("prepeekpostprocesscommand");
+        STable response = SParseJSONObject(brtester.executeWaitMultipleData({cmd})[0].content);
+        ASSERT_EQUAL(response["prePeekInfo"], "this was returned in prePeekInfo");
+        ASSERT_EQUAL(response["postProcessInfo"], "this was returned in postProcessInfo");
     }
 
 } __PrePeekPostProcessTest;
