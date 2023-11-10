e9c6f6638fa665513385362e921717d3f9f0fb82
Tyler Karaszewski <tyler@expensify.com>
2023-11-10 12:12:53 -0800

    Block the CURRENT_TIMESTAMP function in writes.

--- test/tests/WriteTest.cpp
+++ test/tests/WriteTest.cpp
@@ -18,6 +18,7 @@ struct WriteTest : tpunit::TestFixture {
                               TEST(WriteTest::updateAndInsertWithHttp),
                               TEST(WriteTest::shortHandSyntax),
                               TEST(WriteTest::keywordsAsValue),
+                              TEST(WriteTest::blockTimeFunctions),
                               AFTER_CLASS(WriteTest::tearDown)) { }
 
     BedrockTester* tester;
@@ -181,6 +182,20 @@ struct WriteTest : tpunit::TestFixture {
         // Change the expected result to "502 Query aborted" once https://github.com/Expensify/Expensify/issues/165207 is solved
         SData query3("query: UPDATE stuff SET info = 'This is not a where clause';");
         tester->executeWaitVerifyContent(query3);
+    }
+
+    void blockTimeFunctions() {
+        // Verify writing the string 'CURRENT_TIMESTAMP' is fine.
+        SData query("query: INSERT INTO stuff VALUES ( NULL, 11, 'CURRENT_TIMESTAMP' );");
+        tester->executeWaitVerifyContent(query);
+
+        // But verify calling the function CURRENT_TIMESTAMP is blocked when writing.
+        query.methodLine = "query: INSERT INTO stuff VALUES ( NULL, 11, CURRENT_TIMESTAMP );";
+        tester->executeWaitVerifyContent(query, "502 Query failed");
+
+        // But allow the function to run in reads.
+        query.methodLine = "query: SELECT CURRENT_TIMESTAMP;";
+        tester->executeWaitVerifyContent(query);
     }
 
 } __WriteTest;
