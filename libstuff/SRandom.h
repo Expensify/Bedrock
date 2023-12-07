94dc0387683b4a1fb6967c67403d9d1608ac3165
Tyler Karaszewski <tyler@expensify.com>
2023-12-07 16:40:39 -0700

    Allocate when called, not at startup

--- libstuff/SRandom.h
+++ libstuff/SRandom.h
@@ -11,8 +11,8 @@ class SRandom {
     static uint64_t rand64();
     static uint64_t limitedRand64(uint64_t min, uint64_t max);
     static string randStr(uint& length);
+    void init();
 
   private:
-    static mt19937_64 _generator;
     static uniform_int_distribution<uint64_t> _distribution64;
 };
