df8178233b0e8f565539ed8e060a8d7a92be4b8e
Tyler Karaszewski <tyler@expensify.com>
2024-02-21 16:40:09 -0800

    Better output

--- test/lib/PrintEquality.h
+++ test/lib/PrintEquality.h
@@ -12,29 +12,29 @@ using namespace std;
 template<typename T>
 ostream& operator<<(ostream& output, const list<T>& val)
 {
-    return output << "[LIST]";
+    return output << "[" << SComposeList(val) << "]";
 }
 
 template<typename T>
 ostream& operator<<(ostream& output, const set<T>& val)
 {
-    return output << "[SET]";
+    return output << "[" << SComposeList(val) << "]";
 }
 
 template<typename T, typename U>
 ostream& operator<<(ostream& output, const map<T, U>& val)
 {
-    return output << "[MAP]";
+    output << "[Map]{" << endl;
+    for (const auto& [k, v] : val) {
+        output << k << ": " << v << endl;
+    }
+    return output << "}" << endl;
 }
 
 class PrintEquality {
     public:
         template <typename U, typename V>
-        PrintEquality(U a, V b, bool isEqual) {
-            if (!isEqual) {
-                cout << "Not equal " << a << ", " << b << "\n";
-            } else {
-                cout << "equal " << a << ", " << b << "\n";
-            }
+        PrintEquality(const U& a, const V& b, bool isEqual) {
+            cout << (isEqual ? "" : "not ") << "equal " << a << ", " << b << "\n";
         }
 };
