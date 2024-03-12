9b3f2aa348a17ae7ecddefc8535c175fb9d72c69
Tyler Karaszewski <tyler@expensify.com>
2024-03-12 14:26:13 -0700

    Fix test

--- test/clustertest/tests/MassiveQueryTest.cpp
+++ test/clustertest/tests/MassiveQueryTest.cpp
@@ -14,13 +14,31 @@ struct MassiveQueryTest : tpunit::TestFixture {
         cmd["processTimeout"] = "290000";
         cmd["writeConsistency"] = "ASYNC";
         auto r1 = brtester.executeWaitMultipleData({cmd})[0];
-        uint64_t commitCount = stoull(r1["CommitCount"]);
+        uint64_t commitCount = 0;
+        try {
+            commitCount = stoull(r1["CommitCount"]);
+        } catch (const invalid_argument& e) {
+            cout << "invalid_argument parsing commitCount from: " << r1["CommitCount"] << endl;
+        } catch (const out_of_range& e) {
+            cout << "out_of_range parsing commitCount from: " << r1["CommitCount"] << endl;
+        }
         uint64_t commitCount2 = 0;
+
+        // Make sure the commit count is actually set.
+        ASSERT_TRUE(commitCount);
 
         SData status("Status");
         for (size_t i = 0; i < 500; i++) {
             auto r2 = tester.getTester(2).executeWaitMultipleData({status})[0];
-            commitCount2 = stoull(SParseJSONObject(r2.content)["CommitCount"]);
+            try {
+                commitCount2 = stoull(r1["CommitCount"]);
+            } catch (const invalid_argument& e) {
+                cout << "invalid_argument parsing commitCount2 from: " << SParseJSONObject(r2.content)["CommitCount"] << endl;
+                cout << r2.content << endl;
+            } catch (const out_of_range& e) {
+                cout << "out_of_range parsing commitCount2 from: " << SParseJSONObject(r2.content)["CommitCount"] << endl;
+                cout << r2.content << endl;
+            }
             if (commitCount2 == commitCount) {
                 break;
             }
