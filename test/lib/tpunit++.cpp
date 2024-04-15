d3d8b16222fb7199dc14e2cd96b72b3217f72a5d
John Lee <john@expensify.com>
2024-04-15 14:45:00 -0400

    Comments for the wicked

--- test/lib/tpunit++.cpp
+++ test/lib/tpunit++.cpp
@@ -59,6 +59,8 @@ tpunit::TestFixture::TestFixture(method* m0,  method* m1,  method* m2,  method* 
 {
     tpunit_detail_fixture_list()->push_back(this);
 
+    // DO NOT modify this over 70, you're holding it wrong if you do.
+    // Split your test suites/files if you need to!
     method* methods[70] = { m0,  m1,  m2,  m3,  m4,  m5,  m6,  m7,  m8,  m9,
                             m10, m11, m12, m13, m14, m15, m16, m17, m18, m19,
                             m20, m21, m22, m23, m24, m25, m26, m27, m28, m29,
