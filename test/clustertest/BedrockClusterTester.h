12e34d915cc466aa167edc16b0480ba5e34a97f5
Tyler Karaszewski <tyler@expensify.com>
2024-04-18 15:37:16 -0700

    Update test to wait for last commands

--- test/clustertest/BedrockClusterTester.h
+++ test/clustertest/BedrockClusterTester.h
@@ -193,6 +193,15 @@ ClusterTester<T>::~ClusterTester()
 
     // Shut down everything but the leader first.
     list<thread> threads;
+
+    for (int i = 0; i< _size; i++) {
+        // Wait for all the commands to be done before stopping.
+        // If we don't do this, then if the leader loses quorum, because the other nodes have all shut down,
+        // it won't run remaining commands, and it will get stuck forever.
+        // In tests, this can happen with commands that generate other commands that don't expect a response.
+        getTester(i).waitForStatusTerm("commandCount", "1");
+    }
+
     for (int i = _size - 1; i > 0; i--) {
         threads.emplace_back([&, i](){
             stopNode(i);
