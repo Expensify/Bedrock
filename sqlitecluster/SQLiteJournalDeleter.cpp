72bbfc1ea15ef20a0e133e3e8c8739386268e814
Tyler Karaszewski <tyler@expensify.com>
2024-05-07 16:59:14 -0700

    Trying to make this work

--- sqlitecluster/SQLiteJournalDeleter.cpp
+++ sqlitecluster/SQLiteJournalDeleter.cpp
@@ -33,24 +33,35 @@ void SQLiteJournalDeleter::deleteEntries() {
     while (!_stop) {
         results.clear();
         uint64_t commitCount = _commitCountHandle.getCommitCount();
+        uint64_t limit = _limits[_nextTable].first;
         const string& table = _limits[_nextTable].second;
-        uint64_t max = commitCount - _limits[_nextTable].first;
-        int success = SQuery(_db, "cleanup", "SELECT COUNT(*) FROM " + table + " WHERE id > " + SQ(max), results);
-        if (success != SQLITE_OK) {
-            SWARN("Error checking rows to delete from table: " << table);
-            continue;
-        }
 
-        uint64_t toDelete = stoull(results[0][0]);
-        if (toDelete) {
-            SINFO("Deleting " << toDelete << " rows from " << table);
-            success = SQuery(_db, "cleanup", "DELETE FROM " + table + " WHERE id > " + SQ(max), results);
-            if (success == SQLITE_BUSY_SNAPSHOT) {
-                SINFO("Conflict deleting from table " << table);
-            } else if (success != SQLITE_OK) {
-                SWARN("Error deleting from table " << table);
-                continue;
+        try { 
+            // If there's not enough rows to bother deleting, just skip.
+            if (commitCount <= limit) {
+                STHROW("Not enough rows");
             }
+
+            uint64_t max = commitCount - limit;
+            int success = SQuery(_db, "cleanup", "SELECT COUNT(*) FROM " + table + " WHERE id > " + SQ(max), results);
+            if (success != SQLITE_OK) {
+                SWARN("Error checking rows to delete from table: " << table);
+                STHROW("Read error");
+            }
+
+            uint64_t toDelete = stoull(results[0][0]);
+            if (toDelete) {
+                SINFO("Deleting " << toDelete << " rows from " << table);
+                success = SQuery(_db, "cleanup", "DELETE FROM " + table + " WHERE id > " + SQ(max), results);
+                if (success == SQLITE_BUSY_SNAPSHOT) {
+                    SINFO("Conflict deleting from table " << table);
+                } else if (success != SQLITE_OK) {
+                    SWARN("Error deleting from table " << table);
+                    STHROW("Delete error");
+                }
+            }
+        } catch (const SException& e) {
+            // just go on to the next iteration without skipping incrementing the table.
         }
 
         // Move on to the next table.
