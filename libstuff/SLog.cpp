38e8038620643a4e8d215e5b70d1f7646f338a18
neil-marcellini <neil@expensify.com>
2024-05-29 17:22:47 -0700

    Create a stack trace only if the log level is set

--- libstuff/SLog.cpp
+++ libstuff/SLog.cpp
@@ -5,6 +5,10 @@
 atomic<int> _g_SLogMask(LOG_INFO);
 
 void SLogStackTrace(int level) {
+    // If the level isn't set in the log mask, nothing more to do
+    if (!(_g_SLogMask & (1 << level))) {
+        return;
+    }
     // Output the symbols to the log
     void* callstack[100];
     int depth = backtrace(callstack, 100);
