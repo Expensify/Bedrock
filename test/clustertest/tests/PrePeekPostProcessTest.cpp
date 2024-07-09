70a5b97d575e1fc53ca4fd11dcaf9efdc5f4fe07
John Lee <john@expensify.com>
2024-07-09 11:18:36 -0400

    Revert since it didn't really do anything

--- test/clustertest/tests/PrePeekPostProcessTest.cpp
+++ test/clustertest/tests/PrePeekPostProcessTest.cpp
@@ -1,85 +1,86 @@
 #include <BedrockCommand.h>
 #include <libstuff/SData.h>
-#include "PrePeekPostProcessTest.h"
+#include <test/clustertest/BedrockClusterTester.h>
 
 void checkWithoutThis() {
     ASSERT_EQUAL(1, 1);
 }
 
-PrePeekPostProcessTest::PrePeekPostProcessTest() : tpunit::TestFixture("PrePeekPostProcess",
-    BEFORE_CLASS(PrePeekPostProcessTest::setup),
-    AFTER_CLASS(PrePeekPostProcessTest::teardown),
-    TEST(PrePeekPostProcessTest::prePeek),
-    TEST(PrePeekPostProcessTest::prePeekThrow),
-    TEST(PrePeekPostProcessTest::postProcess),
-    TEST(PrePeekPostProcessTest::prePeekPostProcess))
-{
-    NAME(PrePeekPostProcess);
-}
+struct PrePeekPostProcessTest : tpunit::TestFixture {
+    PrePeekPostProcessTest() : tpunit::TestFixture("PrePeekPostProcess", BEFORE_CLASS(PrePeekPostProcessTest::setup),
+                                                                         AFTER_CLASS(PrePeekPostProcessTest::teardown),
+                                                                         TEST(PrePeekPostProcessTest::prePeek),
+                                                                         TEST(PrePeekPostProcessTest::prePeekThrow),
+                                                                         TEST(PrePeekPostProcessTest::postProcess),
+                                                                         TEST(PrePeekPostProcessTest::prePeekPostProcess)) { }
 
-void setup() {
-    tester = new BedrockClusterTester();
-}
+    BedrockClusterTester* tester;
 
-void teardown() {
-    delete tester;
-}
+    void setup() {
+        tester = new BedrockClusterTester();
+    }
 
-void prePeek()
-{
-    checkWithoutThis();
+    void teardown() {
+        delete tester;
+    }
 
-    BedrockTester& brtester = tester->getTester(1);
-    SData cmd("prepeekcommand");
-    STable response = SParseJSONObject(brtester.executeWaitMultipleData({cmd})[0].content);
+    void prePeek()
+    {
+        checkWithoutThis();
 
-    // Confirm that information returned from prePeek and peek is all in the response.
-    ASSERT_EQUAL(response["prePeekInfo"], "this was returned in prePeekInfo");
-    ASSERT_EQUAL(response["peekInfo"], "this was returned in peekInfo");
+        BedrockTester& brtester = tester->getTester(1);
+        SData cmd("prepeekcommand");
+        STable response = SParseJSONObject(brtester.executeWaitMultipleData({cmd})[0].content);
 
-    // No counted row has been inserted into the test table yet, so the "peekCount" should be zero.
-    ASSERT_EQUAL(response["peekCount"], "0");
-}
+        // Confirm that information returned from prePeek and peek is all in the response.
+        ASSERT_EQUAL(response["prePeekInfo"], "this was returned in prePeekInfo");
+        ASSERT_EQUAL(response["peekInfo"], "this was returned in peekInfo");
 
-void prePeekThrow() {
-    BedrockTester& brtester = tester->getTester(1);
-    SData cmd("prepeekcommand");
-    cmd["shouldThrow"] = "true";
-    brtester.executeWaitVerifyContent({cmd}, "501 ERROR");
-}
+        // No counted row has been inserted into the test table yet, so the "peekCount" should be zero.
+        ASSERT_EQUAL(response["peekCount"], "0");
+    }
 
-void postProcess()
-{
-    BedrockTester& brtester = tester->getTester(1);
-    SData cmd("postprocesscommand");
-    STable response = SParseJSONObject(brtester.executeWaitMultipleData({cmd})[0].content);
+    void prePeekThrow() {
+        BedrockTester& brtester = tester->getTester(1);
+        SData cmd("prepeekcommand");
+        cmd["shouldThrow"] = "true";
+        brtester.executeWaitVerifyContent({cmd}, "501 ERROR");
+    }
 
-    // Confirm that the information returned from peek, process and postProcess is all in the response.
-    ASSERT_EQUAL(response["peekInfo"], "this was returned in peekInfo");
-    ASSERT_EQUAL(response["processInfo"], "this was returned in processInfo");
-    ASSERT_EQUAL(response["postProcessInfo"], "this was returned in postProcessInfo");
+    void postProcess()
+    {
+        BedrockTester& brtester = tester->getTester(1);
+        SData cmd("postprocesscommand");
+        STable response = SParseJSONObject(brtester.executeWaitMultipleData({cmd})[0].content);
 
-    // postprocesscommand inserts a row in the "test" table during the process. We need to make sure that the
-    // inserted row does not exist during peek, and that it does exist during postProcess.
-    ASSERT_EQUAL(response["peekCount"], "0");
-    ASSERT_EQUAL(response["postProcessCount"], "1");
-}
+        // Confirm that the information returned from peek, process and postProcess is all in the response.
+        ASSERT_EQUAL(response["peekInfo"], "this was returned in peekInfo");
+        ASSERT_EQUAL(response["processInfo"], "this was returned in processInfo");
+        ASSERT_EQUAL(response["postProcessInfo"], "this was returned in postProcessInfo");
 
-void prePeekPostProcess ()
-{
-    BedrockTester& brtester = tester->getTester(1);
-    SData cmd("prepeekpostprocesscommand");
-    STable response = SParseJSONObject(brtester.executeWaitMultipleData({cmd})[0].content);
+        // postprocesscommand inserts a row in the "test" table during the process. We need to make sure that the
+        // inserted row does not exist during peek, and that it does exist during postProcess.
+        ASSERT_EQUAL(response["peekCount"], "0");
+        ASSERT_EQUAL(response["postProcessCount"], "1");
+    }
 
-    // Confirm that the information returned from prePeek, peek, process and postProcess is all in the response.
-    ASSERT_EQUAL(response["prePeekInfo"], "this was returned in prePeekInfo");
-    ASSERT_EQUAL(response["peekInfo"], "this was returned in peekInfo");
-    ASSERT_EQUAL(response["processInfo"], "this was returned in processInfo");
-    ASSERT_EQUAL(response["postProcessInfo"], "this was returned in postProcessInfo");
+    void prePeekPostProcess ()
+    {
+        BedrockTester& brtester = tester->getTester(1);
+        SData cmd("prepeekpostprocesscommand");
+        STable response = SParseJSONObject(brtester.executeWaitMultipleData({cmd})[0].content);
 
-    // prepeekpostprocesscommand deletes a row from the "test" table during the process. We need to make sure the
-    // row exists during peek, and that it no longer exists during postProcess.
-    ASSERT_EQUAL(response["peekCount"], "1");
-    ASSERT_EQUAL(response["postProcessCount"], "0");
-}
+        // Confirm that the information returned from prePeek, peek, process and postProcess is all in the response.
+        ASSERT_EQUAL(response["prePeekInfo"], "this was returned in prePeekInfo");
+        ASSERT_EQUAL(response["peekInfo"], "this was returned in peekInfo");
+        ASSERT_EQUAL(response["processInfo"], "this was returned in processInfo");
+        ASSERT_EQUAL(response["postProcessInfo"], "this was returned in postProcessInfo");
+
+        // prepeekpostprocesscommand deletes a row from the "test" table during the process. We need to make sure the
+        // row exists during peek, and that it no longer exists during postProcess.
+        ASSERT_EQUAL(response["peekCount"], "1");
+        ASSERT_EQUAL(response["postProcessCount"], "0");
+    }
+
+} __PrePeekPostProcessTest;
 
