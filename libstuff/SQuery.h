f1b7ce47d4cdbc23f60e9f375af7f6672ff7603f
rory <rory@expensify.com>
2024-07-23 15:28:57 -0700

    Add basic implementation of SQuery::prepare

---
+++ libstuff/SQuery.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "libstuff.h"
+
+#include <string>
+#include <variant>
+
+using namespace std;
+
+class SQuery {
+    using QuerySerializable = variant<const char*, string&, int, unsigned, uint64_t, int64_t, double>;
+    static string prepare(const string& query, const map<string, QuerySerializable>& params);
+};
