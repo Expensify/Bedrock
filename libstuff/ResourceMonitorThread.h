cce5c77d260bf0ccea025f0a03019f0816736dce
danieldoglas <dsilva@expensify.com>
2024-10-03 16:10:02 +0000

    fixing constructor to use forward instead of move

--- libstuff/ResourceMonitorThread.h
+++ libstuff/ResourceMonitorThread.h
@@ -9,7 +9,7 @@ public:
     template<typename _Callable, typename... _Args,
 	     typename = _Require<__not_<is_same<__remove_cvref_t<_Callable>, thread>>>>
       explicit
-    ResourceMonitorThread(_Callable&& __f, _Args&&... __args): thread(move(__f), move(__args...)){
+    ResourceMonitorThread(_Callable&& __f, _Args&&... __args): thread(forward<_Callable>(__f), forward<_Args>(__args)...){
         startTime = STimeNow();
         cpuStartTime = SGetCPUUserTime();
     };
