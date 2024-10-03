fdac10c0914bc53542d9873aec0de281ee57969b
danieldoglas <dsilva@expensify.com>
2024-10-03 18:01:49 +0000

    removing unecessary things

--- libstuff/ResourceMonitorThread.h
+++ libstuff/ResourceMonitorThread.h
@@ -9,10 +9,7 @@ public:
     template<typename F, typename... Args,
 	     typename = _Require<__not_<is_same<__remove_cvref_t<F>, thread>>>>
       explicit
-    ResourceMonitorThread(F&& __f, Args&&... __args): thread(wrapper<F, Args...>, forward<F>(__f), forward<Args>(__args)...){};
-
-    string getThreadName();
-    virtual ~ResourceMonitorThread();
+    ResourceMonitorThread(F&& f, Args&&... args): thread(wrapper<F, Args...>, forward<F>(f), forward<Args>(args)...){};
 
 private:
     static thread_local uint64_t startTime;
@@ -21,11 +18,10 @@ private:
     static void before();
     static void after();
     template<typename F, typename... Args>
-    const static inline function<void(F&& f, Args&&... args)> wrapper = [](F&& f, Args&&... args){
+    inline static function<void(F&& f, Args&&... args)> wrapper = [](F&& f, Args&&... args){
         before();
-        f(args...);
+        std::invoke(std::forward<F>(f), std::forward<Args>(args)...);
         after();
     };
-
 };
 
