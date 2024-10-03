6ebc14d92284c7d4d6e306f32ca0b2f9ab72753a
danieldoglas <dsilva@expensify.com>
2024-10-03 17:35:23 +0000

    finding out why compile is failing, wip

--- libstuff/ResourceMonitorThread.h
+++ libstuff/ResourceMonitorThread.h
@@ -9,7 +9,7 @@ public:
     template<typename F, typename... Args,
 	     typename = _Require<__not_<is_same<__remove_cvref_t<F>, thread>>>>
       explicit
-    ResourceMonitorThread(F&& __f, Args&&... __args): thread(&wrapper<F, Args...>, forward<F>(__f), forward<Args>(__args)...){};
+    ResourceMonitorThread(F&& __f, Args&&... __args): thread(wrapper<F, Args...>, forward<F>(__f), forward<Args>(__args)...){};
 
     string getThreadName();
     virtual ~ResourceMonitorThread();
@@ -28,3 +28,4 @@ private:
     };
 
 };
+
