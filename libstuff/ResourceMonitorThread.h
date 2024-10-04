6275f2b63807ac0427d5cd467da1de873f2fd026
danieldoglas <dsilva@expensify.com>
2024-10-04 14:47:58 +0000

    adding comments

--- libstuff/ResourceMonitorThread.h
+++ libstuff/ResourceMonitorThread.h
@@ -3,9 +3,15 @@
 
 using namespace std;
 
+// This class is a wrapper around the default thread. We use it to collect the thread CPU usage. That allows us
+// to investigate if we have any threads using more resources than it should, which can cause CPU usage peaks in
+// the cluster.
 class ResourceMonitorThread: public thread
 {
 public:
+    // When calling this constructor, if you're passing a class member function as the `f` parameter and that
+    // function receives parameters, you will need to wrap your function call in a lambda, doing something like:
+    // ResourceMonitorThread([=, this]{ this->memberFunction(param1, param2);});
     template<typename F, typename... Args>
     ResourceMonitorThread(F&& f, Args&&... args):
       thread(ResourceMonitorThread::wrapper<F&&, Args&&...>, forward<F&&>(f), forward<Args&&>(args)...){};
