2337fd3a7cdd52a2bb0bd222750e6239f69385b4
danieldoglas <dsilva@expensify.com>
2024-10-04 16:32:19 +0000

    removing limiter on logs

--- libstuff/ResourceMonitorThread.cpp
+++ libstuff/ResourceMonitorThread.cpp
@@ -21,12 +21,6 @@ void ResourceMonitorThread::after(){
         return;
     }
     const double cpuUserPercentage = round((CPUUserTime / static_cast<double>(threadEndTime)) * 100 * 1000) / 1000;
-
-    // Any thread that takes more than 100ms and uses more than 50% CPU during it's lifetime will be logged. These parameters
-    // are completely arbitrary, and might need some fine-tuning in the future based on our future investigations.
-    // If we don't add this, we'll log millions log log lines per hour that won't be useful to investigate anything.
-    if (CPUUserTime > 100'000 && cpuUserPercentage > 50) {
-        const pid_t tid = syscall(SYS_gettid);
-        SINFO(format("Thread finished. pID: '{}', CPUTime: '{}µs', CPUPercentage: '{}%'", tid, CPUUserTime, cpuUserPercentage, startTime, cpuStartTime));
-    }
+    const pid_t tid = syscall(SYS_gettid);
+    SINFO(format("Thread finished. pID: '{}', CPUTime: '{}µs', CPUPercentage: '{}%'", tid, CPUUserTime, cpuUserPercentage, startTime, cpuStartTime));
 }
