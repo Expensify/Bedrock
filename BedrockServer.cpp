8bea5c59e3e789d2a6fc381e547d3f509d4e98ad
Tyler Karaszewski <tyler@expensify.com>
2024-10-31 13:33:10 -0700

    Debugging testing code

--- BedrockServer.cpp
+++ BedrockServer.cpp
@@ -7,6 +7,8 @@
 #include <sys/resource.h>
 #include <sys/time.h>
 
+#include <iostream>
+
 #include <bedrockVersion.h>
 #include <BedrockCore.h>
 #include <BedrockPlugin.h>
@@ -1222,6 +1224,10 @@ BedrockServer::BedrockServer(const SData& args_)
     _lastQuorumCommandTime(STimeNow()), _pluginsDetached(false), _socketThreadNumber(0),
     _outstandingSocketThreads(0), _shouldBlockNewSocketThreads(false), _upgradeCompleted(false)
 {
+    if (args.isSet("-waitForDebugger")) {
+        cout << "WAITING FOR DEBUGGER" << endl;
+        sleep(30);
+    }
     _version = VERSION;
 
     // Enable the requested plugins, and update our version string if required.
