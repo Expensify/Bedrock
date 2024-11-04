eb9d55cd5edd2b12655340602e102565e97b70e0
Tyler Karaszewski <tyler@expensify.com>
2024-11-04 10:16:27 -0800

    Re-add test code

--- test/lib/BedrockTester.cpp
+++ test/lib/BedrockTester.cpp
@@ -16,7 +16,7 @@
 PortMap BedrockTester::ports;
 mutex BedrockTester::_testersMutex;
 set<BedrockTester*> BedrockTester::_testers;
-const bool BedrockTester::ENABLE_HCTREE{false};
+const bool BedrockTester::ENABLE_HCTREE{true};
 
 string BedrockTester::getTempFileName(string prefix) {
     string templateStr = "/tmp/" + prefix + "bedrocktest_XXXXXX.db";
