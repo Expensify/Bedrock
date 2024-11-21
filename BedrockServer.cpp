4065792ee5caf730e1e5d083173f0b44b2fc020f
danieldoglas <dsilva@expensify.com>
2024-11-21 21:51:01 +0000

    improving logs

--- BedrockServer.cpp
+++ BedrockServer.cpp
@@ -471,7 +471,7 @@ void BedrockServer::sync()
                 // like a segfault. Note that it's possible we're in the middle of sending a message to peers when we call
                 // this, which would probably make this message malformed. This is the best we can do.
                 SSetSignalHandlerDieFunc([&](){
-                    SHMMM("Sending CRASH_COMMAND from sync", command->getLogParams());
+                    SHMMM("CRASHING from BedrockServer::sync", command->getLogParams());
                     _clusterMessenger->runOnAll(_generateCrashMessage(command));
                 });
 
@@ -700,7 +700,7 @@ void BedrockServer::runCommand(unique_ptr<BedrockCommand>&& _command, bool isBlo
     // If a signal is caught on this thread, which should only happen for unrecoverable, yet synchronous
     // signals, like SIGSEGV, this function will be called.
     SSetSignalHandlerDieFunc([&](){
-        SHMMM("Sending CRASH_COMMAND from BedrockServer::runCommand", command->getLogParams());
+        SHMMM("CRASHING from BedrockServer::runCommand", command->getLogParams());
         _clusterMessenger->runOnAll(_generateCrashMessage(command));
     });
 
