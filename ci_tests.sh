9310e263d8fb01c7bd15824703ae9d9eece24f65
Tyler Karaszewski <tyler@expensify.com>
2024-12-16 17:43:46 -0800

    Binary search the tests, second 1/8th.

--- ci_tests.sh
+++ ci_tests.sh
@@ -48,7 +48,7 @@ mark_fold end test_bedrock
 
 mark_fold start test_bedrock_cluster
 cd test/clustertest
-./clustertest -threads 1 -only BadCommand,BroadcastCommand,ClusterUpgrade,ConflictSpam
+./clustertest -threads 1 -only ControlCommand,DoubleDetach,Escalate
 cd ../..
 mark_fold end test_bedrock_cluster
 
