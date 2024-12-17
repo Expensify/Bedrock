e1e7c0d474a64cc719e3776e511543b0d21f9bab
Tyler Karaszewski <tyler@expensify.com>
2024-12-17 08:20:43 -0800

    Needs only, not except

--- ci_tests.sh
+++ ci_tests.sh
@@ -48,7 +48,7 @@ mark_fold end test_bedrock
 
 mark_fold start test_bedrock_cluster
 cd test/clustertest
-./clustertest -threads 1 -except DoubleDetach
+./clustertest -threads 1 -only DoubleDetach
 # ControlCommand,DoubleDetach,Escalate
 cd ../..
 mark_fold end test_bedrock_cluster
