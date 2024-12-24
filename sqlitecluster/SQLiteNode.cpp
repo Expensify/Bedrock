a47789745b3a4cb5f06c63be79e3c85f00520c3c
danieldoglas <dsilva@expensify.com>
2024-12-24 19:10:29 +0000

    addressing comments

--- sqlitecluster/SQLiteNode.cpp
+++ sqlitecluster/SQLiteNode.cpp
@@ -1289,6 +1289,8 @@ void SQLiteNode::_onMESSAGE(SQLitePeer* peer, const SData& message) {
             _server.blockCommandPort(blockReason);
             _blockedCommandPort = true;
         } else if (currentCommitDifference < 1'000 && _blockedCommandPort) {
+            // We'll open the command port again if we're 1k commits behind, which
+            // translates to ~2s of commits.
             SINFO("Node is caught up enough (behind by " + SToStr(currentCommitDifference) + " commits), re-opening command port.");
             _server.unblockCommandPort(blockReason);
             _blockedCommandPort = false;
