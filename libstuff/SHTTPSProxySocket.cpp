3fd3aae231cecc1d4445159dd5c232d94ce46c4c
Tyler Karaszewski <tyler@expensify.com>
2025-04-01 15:03:47 -0700

    Add proxyAddress to constructors

--- libstuff/SHTTPSProxySocket.cpp
+++ libstuff/SHTTPSProxySocket.cpp
@@ -1,19 +1,21 @@
 #include "SHTTPSProxySocket.h"
 #include "libstuff/STCPManager.h"
 
-SHTTPSProxySocket::SHTTPSProxySocket(const string& host, bool https)
- : STCPManager::Socket(host, https)
+SHTTPSProxySocket::SHTTPSProxySocket(const string& proxyAddress, const string& host, bool https)
+ : STCPManager::Socket(host, https), proxyAddress(proxyAddress)
 {
 }
 
-SHTTPSProxySocket::SHTTPSProxySocket(int sock, State state_, bool https)
- : STCPManager::Socket(sock, state_, https)
+SHTTPSProxySocket::SHTTPSProxySocket(const string& proxyAddress, int sock, State state_, bool https)
+ : STCPManager::Socket(sock, state_, https), proxyAddress(proxyAddress)
 {
 }
 
 SHTTPSProxySocket::SHTTPSProxySocket(SHTTPSProxySocket&& from)
 : STCPManager::Socket(move(from))
 {
+    proxyAddress = move(from.proxyAddress);
+    from.proxyAddress = "";
 }
 
 SHTTPSProxySocket::~SHTTPSProxySocket() {
--- libstuff/SHTTPSProxySocket.h
+++ libstuff/SHTTPSProxySocket.h
@@ -1,11 +1,19 @@
 #include <libstuff/STCPManager.h>
 
 class SHTTPSProxySocket : public STCPManager::Socket {
+  public:
     // Implement all the same constructors as the base class.
-    SHTTPSProxySocket(const string& host, bool https = false);
-    SHTTPSProxySocket(int sock = 0, State state_ = CONNECTING, bool https = false);
+    SHTTPSProxySocket(const string& proxyAddress, const string& host, bool https = false);
+    SHTTPSProxySocket(const string& proxyAddress, int sock = 0, State state_ = CONNECTING, bool https = false);
     SHTTPSProxySocket(SHTTPSProxySocket&& from);
 
     ~SHTTPSProxySocket();
 
+    private:
+
+    // This should contain the address and port, i.e.:
+    // www.proxy.com:443
+    // or:
+    // 127.0.0.1:443
+    string proxyAddress;
 };
