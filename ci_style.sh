449bfb540916d22e2cac08c32eb767e31903bf43
Jairo Calmon <jairocalmon@gmail.com>
2025-12-08 14:53:10 -0300

    Rename branch variable

--- ci_style.sh
+++ ci_style.sh
@@ -4,17 +4,17 @@ set -e
 LOCAL_PATH=$(pwd)
 git config --global --add safe.directory $LOCAL_PATH
 
-# If the TRAVIS_BRANCH env variable is not set to main, fetch the main branch so we can compare against it
-if [[ "$TRAVIS_BRANCH" != "main" ]]; then
+# If the GITHUB_BRANCH env variable is not set to main, fetch the main branch so we can compare against it
+if [[ "$GITHUB_BRANCH" != "main" ]]; then
     git fetch origin main:main
 fi
 
-# Run 'git fetch origin' so that we have a local branch called $TRAVIS_BRANCH that we can compare against
+# Run 'git fetch origin' so that we have a local branch called $GITHUB_BRANCH that we can compare against
 # Only if it starts with pull/
-if [[ "$TRAVIS_BRANCH" == pull/* ]]; then
+if [[ "$GITHUB_BRANCH" == pull/* ]]; then
     # We set --update-head-ok so that if the branch already exists, it will update the branch to the latest commit,
     # even if head is already pointing to it.
-    git fetch origin "${TRAVIS_BRANCH}":"${TRAVIS_BRANCH}" --update-head-ok
+    git fetch origin "${GITHUB_BRANCH}":"${GITHUB_BRANCH}" --update-head-ok
 
     # IF this failed, exit early
     if [[ $? -ne 0 ]]; then
@@ -61,7 +61,7 @@ do
         EXIT_VAL=$RETURN_VAL
     fi
 
-done < <(git diff --name-status --diff-filter=AM main..."${TRAVIS_BRANCH}" -- '*.cpp' '*.h')
+done < <(git diff --name-status --diff-filter=AM main..."${GITHUB_BRANCH}" -- '*.cpp' '*.h')
 
 if [ $EXIT_VAL -gt 0 ]; then
     echo -e "${RED}Style is wrong, run 'vssh ./style.sh' from your Mac, or just './style.sh' from your VM to fix it.${RESET}"
