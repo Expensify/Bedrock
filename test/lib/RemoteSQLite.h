cda1a58ba7d8bf709baf36faa613ecd34686aee3
Jairo Calmon <jairocalmon@gmail.com>
2026-01-29 02:06:49 -0300

    Add basic documentation for RemoteSQLite

--- test/lib/RemoteSQLite.h
+++ test/lib/RemoteSQLite.h
@@ -4,6 +4,10 @@
 
 class BedrockTester;
 
+// RemoteSQLite is a client-side Proxy implementation of the SQLite base class.
+// Instead of performing operations on a local database file, this class serializes read and write operations into `Query` commands
+// and forwards them to a remote server for execution. Consequently, the actual database logic is handled by the remote host.
+// This is particularly useful for testing in environments where multiple processes cannot access the database simultaneously (HCTree).
 class RemoteSQLite : public SQLite {
 public:
     RemoteSQLite(BedrockTester* tester);
