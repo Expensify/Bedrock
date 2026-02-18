7c236372ac3d63d70ffef82c3e90c11b9ce1f589
Rushat Gabhane <rushatgabhane@gmail.com>
2026-02-18 14:07:10 +0100

    add fluentd logger class with socket sender

---
+++ libstuff/SFluentdLogger.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <atomic>
+#include <string>
+#include <thread>
+
+#include <libstuff/SRingBuffer.h>
+
+using namespace std;
+
+class SFluentdLogger {
+  public:
+    SFluentdLogger(const string& host, int port);
+    ~SFluentdLogger();
+
+    bool log(string&& json);
+
+  private:
+    int openSocket();
+    bool sendAll(int fd, const string& data);
+    void senderLoop();
+
+    string host;
+    int port;
+    atomic<bool> running{false};
+    SRingBuffer<string, SRINGBUFFER_DEFAULT_CAPACITY> buffer;
+    thread senderThread;
+};
