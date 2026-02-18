af0a51f2b5848787768f6fe44389340dc99f3362
Rushat Gabhane <rushatgabhane@gmail.com>
2026-02-18 14:07:10 +0100

    add fluentd logger with socket sender thread

---
+++ libstuff/SFluentdLogger.cpp
@@ -0,0 +1,83 @@
+#include <libstuff/SFluentdLogger.h>
+#include <libstuff/SThread.h>
+
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <syslog.h>
+#include <unistd.h>
+
+SFluentdLogger::SFluentdLogger(const string& host, int port) : host(host), port(port), running(true) {
+    auto [thread, future] = SThread(&SFluentdLogger::senderLoop, this);
+    senderThread = move(thread);
+}
+
+SFluentdLogger::~SFluentdLogger() {
+    running.store(false);
+    if (senderThread.joinable()) {
+        senderThread.join();
+    }
+}
+
+int SFluentdLogger::openSocket() {
+    int fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (fd == -1) {
+        return -1;
+    }
+
+    struct sockaddr_in addr = {};
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port);
+    inet_pton(AF_INET, host.data(), &addr.sin_addr);
+
+    if (connect(fd, (struct sockaddr*)&addr, sizeof(addr)) == -1) {
+        close(fd);
+        return -1;
+    }
+
+    return fd;
+}
+
+bool SFluentdLogger::sendAll(int fd, const string& data) {
+    size_t sent = 0;
+    while (sent < data.size()) {
+        ssize_t n = send(fd, data.data() + sent, data.size() - sent, MSG_NOSIGNAL);
+        if (n <= 0) {
+            return false;
+        }
+        sent += n;
+    }
+    return true;
+}
+
+void SFluentdLogger::senderLoop() {
+    int fd = -1;
+
+    while (true) {
+        auto entry = buffer.pop();
+
+        if (!entry.has_value()) {
+            if (!running.load()) {
+                break;
+            }
+            this_thread::sleep_for(chrono::milliseconds(1));
+            continue;
+        }
+
+        if (fd == -1) {
+            fd = openSocket();
+        }
+
+        if (!sendAll(fd, entry.value())) {
+            close(fd);
+            fd = -1;
+            syslog(LOG_WARNING, "%s", entry.value().data());
+        }
+    }
+
+    if (fd != -1) {
+        close(fd);
+    }
+}
+
+bool SFluentdLogger::log(string&& json) { return buffer.push(move(json)); }
