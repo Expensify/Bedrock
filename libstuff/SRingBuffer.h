4e62245efaaa406370627754521832d890260263
Rushat Gabhane <rushatgabhane@gmail.com>
2026-02-20 21:58:49 +0100

    replace ready flag with state enum; add wait/shutdown

--- libstuff/SRingBuffer.h
+++ libstuff/SRingBuffer.h
@@ -4,11 +4,19 @@
 #include <atomic>
 #include <optional>
 #include <string>
+#include <utility>
 
 using namespace std;
 
 // 10M items, ~1GB at 100 bytes/item, ~200 seconds buffer at 50K items/sec
 constexpr size_t SRINGBUFFER_DEFAULT_CAPACITY = 10'000'000;
+
+enum class State
+{
+    Empty,
+    Ready,
+    Shutdown
+};
 
 /*
  * Lock free multi producer, single consumer ring buffer. Used for Fluentd logging
@@ -18,9 +26,7 @@ public:
     struct BufferElement
     {
         T data;
-
-        // Prevents consumer from reading partially-written data. Producer reserves slot with CAS, then writes data, then signals ready.
-        atomic<bool> isReady{false};
+        atomic<State> state{State::Empty};
     };
 
     bool push(T&& data)
@@ -38,26 +44,59 @@ public:
 
         size_t index = currentWriteIndex % C;
         buffer[index].data = move(data);
-        buffer[index].isReady.store(true, memory_order_release);
+        buffer[index].state.store(State::Ready, memory_order_release);
+        buffer[index].state.notify_one();
 
         return true;
     }
 
-    optional<T> pop()
+    pair<optional<T>, State> pop()
     {
-        size_t currentReadIndex = readIndex.load();
+        size_t currentReadIndex = readIndex.load(memory_order_acquire);
         size_t index = currentReadIndex % C;
 
-        if (!buffer[index].isReady.load(memory_order_acquire)) {
-            return nullopt;
+        State slotState = buffer[index].state.load(memory_order_acquire);
+
+        if (slotState == State::Empty) {
+            return {nullopt, State::Empty};
+        }
+
+        if (slotState == State::Shutdown) {
+            buffer[index].state.store(State::Empty, memory_order_release);
+            readIndex.store(currentReadIndex + 1, memory_order_release);
+            return {nullopt, State::Shutdown};
         }
 
         T bufferData = move(buffer[index].data);
-        buffer[index].isReady.store(false, memory_order_release);
+        buffer[index].state.store(State::Empty, memory_order_release);
 
         readIndex.store(currentReadIndex + 1, memory_order_release);
 
-        return bufferData;
+        return {bufferData, State::Ready};
+    }
+
+    void wait()
+    {
+        size_t index = readIndex.load(memory_order_acquire) % C;
+        buffer[index].state.wait(State::Empty, memory_order_acquire);
+    }
+
+    void shutdown()
+    {
+        size_t currentWriteIndex = writeIndex.load(memory_order_relaxed);
+
+        while (true) {
+            while (currentWriteIndex - readIndex.load(memory_order_acquire) >= C) {
+                currentWriteIndex = writeIndex.load(memory_order_relaxed);
+            }
+            if (writeIndex.compare_exchange_weak(currentWriteIndex, currentWriteIndex + 1, memory_order_acq_rel, memory_order_relaxed)) {
+                break;
+            }
+        }
+
+        size_t index = currentWriteIndex % C;
+        buffer[index].state.store(State::Shutdown, memory_order_release);
+        buffer[index].state.notify_one();
     }
 
 private:
