75675d091659c333859b77b0fc8a54c89e9b5a72
Florent De Neve <florent@expensify.com>
2026-05-18 15:27:26 +0200

    Update tests accordingly

--- test/clustertest/tests/TimeoutTest.cpp
+++ test/clustertest/tests/TimeoutTest.cpp
@@ -75,15 +75,15 @@ struct TimeoutTest : tpunit::TestFixture
         // Test write commands.
         BedrockTester& brtester = tester->getTester(0);
 
-        // Run one long query.
+        // One large INSERT that exceeds the default 5s processTimeout on its own.
         SData slow("slowprocessquery");
-        slow["size"] = "1000000";
+        slow["size"] = "10000000";
         slow["count"] = "1";
         brtester.executeWaitVerifyContent(slow, "555 Timeout processing command");
 
-        // And a bunch of faster ones.
-        slow["size"] = "100";
-        slow["count"] = "10000";
+        // Many medium INSERTs that cumulatively exceed the default 5s processTimeout.
+        slow["size"] = "100000";
+        slow["count"] = "100";
         brtester.executeWaitVerifyContent(slow, "555 Timeout processing command");
     }
 
