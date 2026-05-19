a6a1b154cc7606a2f0c1eba0cc3ed0d0d1bcd3f4
Tyler Karaszewski <tyler@expensify.com>
2026-05-18 17:16:00 -0700

    Style

--- libstuff/SQliteParameter.cpp
+++ libstuff/SQliteParameter.cpp
@@ -8,19 +8,23 @@
 string SQliteParameter::serialize() const
 {
     switch (type) {
-    case Type::Null:
-        return "N";
-    case Type::Int64:
-        return "I" + to_string(intValue);
-    case Type::Double: {
-        char buf[32];
-        snprintf(buf, sizeof(buf), "%.17g", doubleValue);
-        return string("D") + buf;
-    }
-    case Type::Text:
-        return "T" + SEncodeBase64(stringValue);
-    case Type::Blob:
-        return "B" + SEncodeBase64(stringValue);
+        case Type::Null:
+            return "N";
+
+        case Type::Int64:
+            return "I" + to_string(intValue);
+
+        case Type::Double: {
+            char buf[32];
+            snprintf(buf, sizeof(buf), "%.17g", doubleValue);
+            return string("D") + buf;
+        }
+
+        case Type::Text:
+            return "T" + SEncodeBase64(stringValue);
+
+        case Type::Blob:
+            return "B" + SEncodeBase64(stringValue);
     }
     return "N";
 }
@@ -32,16 +36,20 @@ SQliteParameter SQliteParameter::deserialize(const string& encoded)
     }
     const string payload = encoded.substr(1);
     switch (encoded[0]) {
-    case 'N':
-        return null();
-    case 'I':
-        return i(strtoll(payload.c_str(), nullptr, 10));
-    case 'D':
-        return d(strtod(payload.c_str(), nullptr));
-    case 'T':
-        return text(SDecodeBase64(payload));
-    case 'B':
-        return blob(SDecodeBase64(payload));
+        case 'N':
+            return null();
+
+        case 'I':
+            return i(strtoll(payload.c_str(), nullptr, 10));
+
+        case 'D':
+            return d(strtod(payload.c_str(), nullptr));
+
+        case 'T':
+            return text(SDecodeBase64(payload));
+
+        case 'B':
+            return blob(SDecodeBase64(payload));
     }
     return null();
 }
