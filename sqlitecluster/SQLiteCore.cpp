0cac7ed72c1506beb6dd8feb9691d97073c7bd0b
Daniel Silva <dsilva@expensify.com>
2026-05-21 14:18:42 -0300

    adding passing down the commit lock timeout to core

--- sqlitecluster/SQLiteCore.cpp
+++ sqlitecluster/SQLiteCore.cpp
@@ -8,14 +8,20 @@ SQLiteCore::SQLiteCore(SQLite& db) : _db(db)
 {
 }
 
-bool SQLiteCore::commit(const SQLiteNode& node, uint64_t& commitID, string& transactionHash, const string& commandName, bool needsPluginNotification, void (*notificationHandler)(SQLite& _db, int64_t tableID)) noexcept
+bool SQLiteCore::commit(const SQLiteNode& node, uint64_t& commitID, string& transactionHash, const string& commandName, bool needsPluginNotification, void (*notificationHandler)(SQLite& _db, int64_t tableID), chrono::microseconds commitLockTimeout) noexcept
 {
     // This handler only needs to exist in prepare so we scope it here to automatically unset
     // the handler function once we are done with prepare.
     {
         AutoScopeOnPrepare onPrepare(needsPluginNotification, _db, notificationHandler);
-        // This should always succeed.
-        SASSERT(_db.prepare(&commitID, &transactionHash));
+
+        // This will fail only if we can't acquire the commit lock respecting the command timeout, which 
+        // likely means our cluster health is not optimal. In this case, we want to roll back and 
+        // return a failure, which will cause SQLiteNode to re-run the command and return the
+        // appropriate timeout exception.
+        if (_db.prepare(&commitID, &transactionHash, commitLockTimeout)) {
+            return false;
+        }
     }
 
     // Check for any state other than leading and refuse.
