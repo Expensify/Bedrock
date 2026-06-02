7c448ecd2629ee81c823dcc488836550b3e70d63
Daniel Silva <dsilva@expensify.com>
2026-06-02 19:46:07 +0000

    style

--- sqlitecluster/SQLiteNode.cpp
+++ sqlitecluster/SQLiteNode.cpp
@@ -2955,8 +2955,8 @@ int SQLiteNode::setPriority(int newPriority)
     _configuredPriority.store(newPriority);
 
     const int oldPriority = _priority;
-    
-    // When oldPriority is -1 or 1, it means we're in a transitional state (STANDDOWN or WAITING) where priority doesn't 
+
+    // When oldPriority is -1 or 1, it means we're in a transitional state (STANDDOWN or WAITING) where priority doesn't
     // matter we'll rewrite it with configuredPriority. Let's keep the old _priority until we transition out of that state.
     // This prevents weird cases where we would broadcast to other peers that we have a high priority when we're not ready
     // to lead, and get stuck in a loop of trying to stand up and then immediately standing down again.
@@ -2964,9 +2964,9 @@ int SQLiteNode::setPriority(int newPriority)
         SINFO("Previous priority indicates that we're either in STANDDOWN or WAITING. Do not change it, it will be changed later.", {{"oldPriority", to_string(oldPriority)}});
         return oldConfiguredPriority;
     }
-    
+
     _priority.store(newPriority);
-    
+
     SData state("STATE");
     state["Priority"] = SToStr(newPriority);
     state["State"] = stateName(_state);
