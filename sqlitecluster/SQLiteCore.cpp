a4b3392ab6995e3b83e4d2dae2119116e6aa5a52
Tyler Karaszewski <tyler@expensify.com>
2026-06-26 11:26:44 -0700

    style

--- sqlitecluster/SQLiteCore.cpp
+++ sqlitecluster/SQLiteCore.cpp
@@ -19,7 +19,7 @@ bool SQLiteCore::commit(const SQLiteNode& node, uint64_t& commitID, string& tran
         // This will fail only if we can't acquire the commit lock respecting the command timeout, which
         // likely means our cluster health is not optimal. In this case, we want to roll back and
         // return false, which will make the caller return the appropriate exception.
-        if(abortPtr) {
+        if (abortPtr) {
             _db.setAbortRef(*abortPtr);
         }
         if (!_db.prepare(&commitID, &transactionHash, commitLockTimeout)) {
