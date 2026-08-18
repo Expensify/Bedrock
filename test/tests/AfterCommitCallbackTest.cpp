c5d95c9e52054fdae58091fbd78a7afbc86f8657
Daniel Silva <dsilva@expensify.com>
2026-08-18 11:48:17 -0300

    add test for afterCommit not firing on rollback

--- test/tests/AfterCommitCallbackTest.cpp
+++ test/tests/AfterCommitCallbackTest.cpp
@@ -23,7 +23,8 @@ struct AfterCommitCallbackTest : tpunit::TestFixture
 {
     AfterCommitCallbackTest()
         : tpunit::TestFixture("AfterCommitCallback",
-                              TEST(AfterCommitCallbackTest::firesOncePerCommit))
+                              TEST(AfterCommitCallbackTest::firesOncePerCommit),
+                              TEST(AfterCommitCallbackTest::doesNotFireOnRollback))
     {
     }
 
@@ -51,6 +52,35 @@ struct AfterCommitCallbackTest : tpunit::TestFixture
             ASSERT_EQUAL(db.commit(), SQLITE_OK);
             ASSERT_EQUAL(callCount.load(), i + 1);
         }
+    }
+
+    void doesNotFireOnRollback()
+    {
+        AfterCommitCallbackTempDBFile dbFile;
+        SQLite db(dbFile.filename, 1000, 1000, 1);
+
+        db.beginTransaction(SQLite::TRANSACTION_TYPE::EXCLUSIVE);
+        db.write("CREATE TABLE testTable(id INTEGER PRIMARY KEY, value INTEGER);");
+        db.prepare();
+        ASSERT_EQUAL(db.commit(), SQLITE_OK);
+
+        // Register only now, so the setup commit above can't be mistaken for the write we roll back.
+        atomic<int> callCount(0);
+        db.registerAfterCommitCallback([&callCount]() {
+            callCount++;
+        });
+
+        db.beginTransaction(SQLite::TRANSACTION_TYPE::EXCLUSIVE);
+        db.write("INSERT INTO testTable VALUES(1, 1);");
+        db.rollback();
+        ASSERT_EQUAL(callCount.load(), 0);
+
+        // A transaction that is prepared and then rolled back also doesn't count.
+        db.beginTransaction(SQLite::TRANSACTION_TYPE::EXCLUSIVE);
+        db.write("INSERT INTO testTable VALUES(2, 2);");
+        db.prepare();
+        db.rollback();
+        ASSERT_EQUAL(callCount.load(), 0);
     }
 
 } __AfterCommitCallbackTest;
