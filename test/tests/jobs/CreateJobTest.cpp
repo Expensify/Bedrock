7f7a0db6b940ac9a228f02275d3691382217280e
Monil Bhavsar <monil@expensify.com>
2026-09-08 18:46:44 +0530

    Test malformed job first run date

--- test/tests/jobs/CreateJobTest.cpp
+++ test/tests/jobs/CreateJobTest.cpp
@@ -17,10 +17,11 @@ struct CreateJobTest : tpunit::TestFixture
                               TEST(CreateJobTest::createWithData),
                               TEST(CreateJobTest::createWithRepeat),
                               TEST(CreateJobTest::uniqueJob),
-                              TEST(CreateJobTest::uniqueJobMergeData),
-                              TEST(CreateJobTest::createWithBadData),
-                              TEST(CreateJobTest::createWithBadRepeat),
-                              TEST(CreateJobTest::createChildWithQueuedParent),
+                               TEST(CreateJobTest::uniqueJobMergeData),
+                               TEST(CreateJobTest::createWithBadData),
+                               TEST(CreateJobTest::createWithBadRepeat),
+                               TEST(CreateJobTest::createWithMalformedFirstRun),
+                               TEST(CreateJobTest::createChildWithQueuedParent),
                               TEST(CreateJobTest::createChildWithRunningGrandparent),
                               TEST(CreateJobTest::retryRecurringJobs),
                               TEST(CreateJobTest::retryWithMalformedValue),
@@ -303,6 +304,14 @@ struct CreateJobTest : tpunit::TestFixture
         command["name"] = "blabla";
         command["repeat"] = "blabla";
         tester->executeWaitVerifyContent(command, "402 Malformed repeat");
+    }
+
+    void createWithMalformedFirstRun()
+    {
+        SData command("CreateJob");
+        command["name"] = "blabla";
+        command["firstRun"] = "2026-01-01 00:10:00(change this to current monday)";
+        tester->executeWaitVerifyContent(command, "402 Malformed firstRun");
     }
 
     // Cannot create a child job when parent is QUEUED
