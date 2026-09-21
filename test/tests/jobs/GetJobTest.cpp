46112647921129174557c35451cde2344fdeb577
Issa Nimaga <inimaga@expensify.com>
2026-09-21 15:26:26 +0100

    Trim job snapshot test changes

--- test/tests/jobs/GetJobTest.cpp
+++ test/tests/jobs/GetJobTest.cpp
@@ -101,7 +101,6 @@ struct GetJobTest : tpunit::TestFixture
         ASSERT_EQUAL(response["jobID"], jobID);
         ASSERT_EQUAL(response["name"], jobName);
         ASSERT_EQUAL(response["data"], "{}");
-        ASSERT_EQUAL(response["expectedData"], "{}");
         ASSERT_EQUAL(response["priority"], originalJob[0][8]);
         ASSERT_EQUAL(response["repeat"], originalJob[0][6]);
         ASSERT_EQUAL(response["nextRun"], originalJob[0][4]);
@@ -136,8 +135,6 @@ struct GetJobTest : tpunit::TestFixture
         command["name"] = jobName;
         const string data = R"({"object":{},"array":[],"nested":{"items":[{},[],null]},"bigint":18446744073709551615,"float":1.0,"text":"日本語 \u00e9 \" \\ \/ \n"})";
         command["data"] = data;
-        command["unique"] = "true";
-        command["rerunIfDataChanged"] = "true";
         STable response = tester->executeWaitVerifyContentTable(command);
         string jobID = response["jobID"];
         ASSERT_GREATER_THAN(stol(jobID), 0);
@@ -150,17 +147,14 @@ struct GetJobTest : tpunit::TestFixture
         command["name"] = jobName;
         const string content = tester->executeWaitVerifyContent(command);
         response = SParseJSONObject(content);
-        const JSON::Value job = JSON::Value::parse(content);
 
         uint64_t end = STimeNow();
 
         ASSERT_EQUAL(response.size(), 10);
         ASSERT_EQUAL(response["jobID"], jobID);
         ASSERT_EQUAL(response["name"], jobName);
-        ASSERT_TRUE(job["data"].isObject());
         ASSERT_EQUAL(response["data"], data);
-        ASSERT_TRUE(job["expectedData"].isString());
-        ASSERT_EQUAL(job["expectedData"].getString(), data);
+        ASSERT_EQUAL(JSON::Value::parse(content)["expectedData"].getString(), data);
         SASSERT(!response["created"].empty());
 
         // Check that nothing changed after we created the job except for the state and lastRun value
@@ -179,14 +173,6 @@ struct GetJobTest : tpunit::TestFixture
         ASSERT_EQUAL(currentJob[0][7], originalJob[0][7]);
         ASSERT_EQUAL(currentJob[0][8], originalJob[0][8]);
         ASSERT_EQUAL(currentJob[0][9], originalJob[0][9]);
-
-        // Passing the snapshot back unchanged finishes the job without a spurious rerun.
-        command.clear();
-        command.methodLine = "FinishJob";
-        command["jobID"] = jobID;
-        command["expectedData"] = job["expectedData"].getString();
-        tester->executeWaitVerifyContent(command);
-        ASSERT_EQUAL(tester->readDB("SELECT COUNT(*) FROM jobs WHERE jobID = " + jobID + ";"), "0");
     }
 
     // Cannot use numResults
