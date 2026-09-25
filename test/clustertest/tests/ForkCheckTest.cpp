1eb0f4dabd557d18525d2e5ad83e5ea7c5adc3b0
Tyler Karaszewski <tyler@expensify.com>
2026-09-25 14:22:35 -0700

    Inspect stopped fork-test databases without Bedrock initialization

--- test/clustertest/tests/ForkCheckTest.cpp
+++ test/clustertest/tests/ForkCheckTest.cpp
@@ -15,28 +15,6 @@ struct ForkCheckTest : tpunit::TestFixture
     {
     }
 
-    uint64_t getMaxJournalCommit(BedrockTester& tester, bool online = true)
-    {
-        const string maxID = tester.readDB("SELECT MAX(id) FROM journalEntries;", online);
-        return maxID.empty() ? 0 : SToUInt64(maxID);
-    }
-
-    string getJournalTableForCommit(BedrockTester& tester, uint64_t commitID, bool online)
-    {
-        SQResult journals;
-        tester.readDB("SELECT name FROM sqlite_schema WHERE type ='table' AND name LIKE 'journal%';", journals, online);
-        for (auto& row : journals) {
-            if (!tester.readDB("SELECT id FROM " + row[0] + " WHERE id = " + SQ(commitID) + ";", online).empty()) {
-                return row[0];
-            }
-        }
-        if (tester.readDB("SELECT COUNT(*) FROM sqlite_schema WHERE type='table' AND name='hct_journal';", online) == "1" &&
-            !tester.readDB("SELECT cid FROM hct_journal WHERE cid = " + SQ(commitID) + ";", online).empty()) {
-            return "hct_journal";
-        }
-        return "";
-    }
-
     vector<thread> createThreads(size_t num, BedrockClusterTester& tester, atomic<bool>& stop, atomic<bool>& leaderIsUp)
     {
         // Just use a bunch of copies of the same command.
@@ -89,30 +67,25 @@ struct ForkCheckTest : tpunit::TestFixture
             t.join();
         }
 
-        // Break the journal on leader intentionally to fake a fork.
-        const uint64_t leaderMaxCommit = getMaxJournalCommit(tester.getTester(0), false);
-        const uint64_t followerMaxCommit = getMaxJournalCommit(tester.getTester(1));
+        const uint64_t followerMaxCommit = SToUInt64(tester.getTester(1).readDB("SELECT MAX(id) FROM journalEntries;"));
 
-        // Make sure the follower got farther than the leader.
-        ASSERT_GREATER_THAN(followerMaxCommit, leaderMaxCommit);
-
-        // A failed HC-Tree leader commit can leave a blank final entry, so corrupt
-        // the most recent nonblank hash rather than assuming the highest CID has one.
-        const string lastHashedID = tester.getTester(0).readDB("SELECT MAX(id) FROM journalEntries WHERE length(hash) > 0;", false);
-        ASSERT_FALSE(lastHashedID.empty());
-        const uint64_t corruptCommit = SToUInt64(lastHashedID);
-        const string journalTable = getJournalTableForCommit(tester.getTester(0), corruptCommit, false);
-        ASSERT_FALSE(journalTable.empty());
-
-        // We need to release any DB that the tester is holding.
-        tester.getTester(0).freeDB();
-
-        // Break leader.
+        // Inspect and corrupt the stopped leader through raw SQLite, without running Bedrock's startup initialization.
         {
             string filename = tester.getTester(0).getArg("-db");
             sqlite3* db = nullptr;
-            ASSERT_EQUAL(sqlite3_open_v2(filename.c_str(), &db, SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE | SQLITE_OPEN_NOMUTEX, NULL), SQLITE_OK);
-            if (journalTable == "hct_journal") {
+            ASSERT_EQUAL(sqlite3_open_v2(filename.c_str(), &db, SQLITE_OPEN_READWRITE | SQLITE_OPEN_NOMUTEX, NULL), SQLITE_OK);
+            unique_ptr<sqlite3, decltype(& sqlite3_close)> connection(db, sqlite3_close);
+            SQResult result;
+            ASSERT_EQUAL(SQuery(db, "SELECT MAX(id) AS id FROM journalEntries;", result), SQLITE_OK);
+            ASSERT_EQUAL(result.size(), 1ul);
+            ASSERT_GREATER_THAN(followerMaxCommit, SToUInt64(result[0]["id"]));
+
+            // A failed HC-Tree leader commit can leave a blank final entry, so corrupt the latest nonblank hash.
+            ASSERT_EQUAL(SQuery(db, "SELECT MAX(id) AS id FROM journalEntries WHERE length(hash) > 0;", result), SQLITE_OK);
+            ASSERT_EQUAL(result.size(), 1ul);
+            ASSERT_FALSE(result[0]["id"].empty());
+            const uint64_t corruptCommit = SToUInt64(result[0]["id"]);
+            if (BedrockTester::ENABLE_HCTREE) {
                 sqlite3_stmt* stmt = nullptr;
                 ASSERT_EQUAL(sqlite3_prepare_v2(db, "SELECT query FROM hct_journal WHERE cid = ?", -1, &stmt, nullptr), SQLITE_OK);
                 sqlite3_bind_int64(stmt, 1, corruptCommit);
@@ -131,11 +104,16 @@ struct ForkCheckTest : tpunit::TestFixture
                 ASSERT_EQUAL(sqlite3_changes(db), 1);
                 ASSERT_EQUAL(sqlite3_finalize(stmt), SQLITE_OK);
             } else {
-                string query = "UPDATE " + journalTable + " SET hash = 'abcdef123456' WHERE id = " + SQ(corruptCommit) + ";";
-                ASSERT_EQUAL(sqlite3_exec(db, query.c_str(), nullptr, nullptr, nullptr), SQLITE_OK);
-                ASSERT_EQUAL(sqlite3_changes(db), 1);
+                SQResult journals;
+                ASSERT_EQUAL(SQuery(db, "SELECT name FROM sqlite_schema WHERE type='table' AND name LIKE 'journal%';", journals), SQLITE_OK);
+                int changed = 0;
+                for (const auto& row : journals) {
+                    const string query = "UPDATE " + row["name"] + " SET hash = 'abcdef123456' WHERE id = " + SQ(corruptCommit) + ";";
+                    ASSERT_EQUAL(sqlite3_exec(db, query.c_str(), nullptr, nullptr, nullptr), SQLITE_OK);
+                    changed += sqlite3_changes(db);
+                }
+                ASSERT_EQUAL(changed, 1);
             }
-            ASSERT_EQUAL(sqlite3_close_v2(db), SQLITE_OK);
         }
 
         // Start the broken leader back up. We expect it will fail to synchronize.
