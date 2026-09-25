c8f910f20966080d457bdde935adffaf583b7b3f
Tyler Karaszewski <tyler@expensify.com>
2026-09-25 11:18:58 -0700

    Corrupt the HC-Tree hash in fork detection tests

--- test/clustertest/tests/ForkCheckTest.cpp
+++ test/clustertest/tests/ForkCheckTest.cpp
@@ -15,26 +15,27 @@ struct ForkCheckTest : tpunit::TestFixture
     {
     }
 
-    pair<uint64_t, string> getMaxJournalCommit(BedrockTester& tester, bool online = true)
+    uint64_t getMaxJournalCommit(BedrockTester& tester, bool online = true)
+    {
+        const string maxID = tester.readDB("SELECT MAX(id) FROM journalEntries;", online);
+        return maxID.empty() ? 0 : SToUInt64(maxID);
+    }
+
+    string getJournalTableForCommit(BedrockTester& tester, uint64_t commitID, bool online)
     {
         SQResult journals;
         tester.readDB("SELECT name FROM sqlite_schema WHERE type ='table' AND name LIKE 'journal%';", journals, online);
-        uint64_t maxJournalCommit = 0;
-        string maxJournalTable;
         for (auto& row : journals) {
-            string maxID = tester.readDB("SELECT MAX(id) FROM " + row[0] + ";", online);
-            try {
-                uint64_t maxCommitNum = stoull(maxID);
-                if (maxCommitNum > maxJournalCommit) {
-                    maxJournalCommit = maxCommitNum;
-                    maxJournalTable = row[0];
-                }
-            } catch (const invalid_argument& e) {
-                // do nothing, skip this journal with no entries.
-                continue;
+            if (!tester.readDB("SELECT id FROM " + row[0] + " WHERE id = " + SQ(commitID) + ";", online).empty()) {
+                // journalEntries prefers the legacy row if the two journals overlap.
+                return row[0];
             }
         }
-        return make_pair(maxJournalCommit, maxJournalTable);
+        if (tester.readDB("SELECT COUNT(*) FROM sqlite_schema WHERE type='table' AND name='hct_journal';", online) == "1" &&
+            !tester.readDB("SELECT cid FROM hct_journal WHERE cid = " + SQ(commitID) + ";", online).empty()) {
+            return "hct_journal";
+        }
+        return "";
     }
 
     vector<thread> createThreads(size_t num, BedrockClusterTester& tester, atomic<bool>& stop, atomic<bool>& leaderIsUp)
@@ -90,15 +91,19 @@ struct ForkCheckTest : tpunit::TestFixture
         }
 
         // Break the journal on leader intentionally to fake a fork.
-        auto result = getMaxJournalCommit(tester.getTester(0), false);
-
-        uint64_t leaderMaxCommit = result.first;
-        string leaderMaxCommitJournal = result.second;
-        result = getMaxJournalCommit(tester.getTester(1));
-        uint64_t followerMaxCommit = result.first;
+        const uint64_t leaderMaxCommit = getMaxJournalCommit(tester.getTester(0), false);
+        const uint64_t followerMaxCommit = getMaxJournalCommit(tester.getTester(1));
 
         // Make sure the follower got farther than the leader.
         ASSERT_GREATER_THAN(followerMaxCommit, leaderMaxCommit);
+
+        // A failed HC-Tree leader commit can leave a blank final entry, so corrupt
+        // the most recent nonblank hash rather than assuming the highest CID has one.
+        const string lastHashedID = tester.getTester(0).readDB("SELECT MAX(id) FROM journalEntries WHERE length(hash) > 0;", false);
+        ASSERT_FALSE(lastHashedID.empty());
+        const uint64_t corruptCommit = SToUInt64(lastHashedID);
+        const string journalTable = getJournalTableForCommit(tester.getTester(0), corruptCommit, false);
+        ASSERT_FALSE(journalTable.empty());
 
         // We need to release any DB that the tester is holding.
         tester.getTester(0).freeDB();
@@ -106,16 +111,32 @@ struct ForkCheckTest : tpunit::TestFixture
         // Break leader.
         {
             string filename = tester.getTester(0).getArg("-db");
-            string query = "UPDATE " + leaderMaxCommitJournal + " SET hash = 'abcdef123456' WHERE id = " + to_string(leaderMaxCommit) + ";";
-
             sqlite3* db = nullptr;
-            sqlite3_open_v2(filename.c_str(), &db, SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE | SQLITE_OPEN_NOMUTEX, NULL);
-            char* errMsg = nullptr;
-            sqlite3_exec(db, query.c_str(), 0, 0, &errMsg);
-            if (errMsg) {
-                cout << "Error updating db: " << errMsg << endl;
+            ASSERT_EQUAL(sqlite3_open_v2(filename.c_str(), &db, SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE | SQLITE_OPEN_NOMUTEX, NULL), SQLITE_OK);
+            if (journalTable == "hct_journal") {
+                sqlite3_stmt* stmt = nullptr;
+                ASSERT_EQUAL(sqlite3_prepare_v2(db, "SELECT query FROM hct_journal WHERE cid = ?", -1, &stmt, nullptr), SQLITE_OK);
+                sqlite3_bind_int64(stmt, 1, corruptCommit);
+                ASSERT_EQUAL(sqlite3_step(stmt), SQLITE_ROW);
+                const char* data = static_cast<const char*>(sqlite3_column_blob(stmt, 0));
+                const int size = sqlite3_column_bytes(stmt, 0);
+                ASSERT_TRUE(data && size >= 74);
+                string changed(data, size);
+                changed[0] = changed[0] == '0' ? '1' : '0';
+                ASSERT_EQUAL(sqlite3_finalize(stmt), SQLITE_OK);
+
+                ASSERT_EQUAL(sqlite3_prepare_v2(db, "UPDATE hct_journal SET query = ? WHERE cid = ?", -1, &stmt, nullptr), SQLITE_OK);
+                sqlite3_bind_blob(stmt, 1, changed.data(), changed.size(), SQLITE_TRANSIENT);
+                sqlite3_bind_int64(stmt, 2, corruptCommit);
+                ASSERT_EQUAL(sqlite3_step(stmt), SQLITE_DONE);
+                ASSERT_EQUAL(sqlite3_changes(db), 1);
+                ASSERT_EQUAL(sqlite3_finalize(stmt), SQLITE_OK);
+            } else {
+                string query = "UPDATE " + journalTable + " SET hash = 'abcdef123456' WHERE id = " + SQ(corruptCommit) + ";";
+                ASSERT_EQUAL(sqlite3_exec(db, query.c_str(), nullptr, nullptr, nullptr), SQLITE_OK);
+                ASSERT_EQUAL(sqlite3_changes(db), 1);
             }
-            sqlite3_close_v2(db);
+            ASSERT_EQUAL(sqlite3_close_v2(db), SQLITE_OK);
         }
 
         // Start the broken leader back up. We expect it will fail to synchronize.
